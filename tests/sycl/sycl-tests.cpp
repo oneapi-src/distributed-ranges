14a6351e398a7f6a658dd3539d8fccd4c29f157d
Robert Cohn <robert.s.cohn@intel.com>
2022-11-03 11:40:49 -0400

    prep for sycl testing (#46)

---
+++ tests/sycl/sycl-tests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include <CL/sycl.hpp>
+
+#include "dr/distributed-ranges.hpp"
+
+MPI_Comm comm;
+int comm_rank;
+int comm_size;
+
+// Demonstrate some basic assertions.
+TEST(CpuTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  auto res = RUN_ALL_TESTS();
+
+  return res;
+}
