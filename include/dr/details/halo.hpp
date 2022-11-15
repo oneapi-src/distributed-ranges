5b360caa869f347aeba50e8b390e6a2d53aa640e
Tuomas Kärnä <tuomas.karna@gmail.com>
2022-11-15 19:47:26 +0200

    Halo reduction (#60)

--- include/dr/details/halo.hpp
+++ include/dr/details/halo.hpp
@@ -3,13 +3,15 @@ namespace lib {
 // Halo consists of multiple segments
 template <typename T> class halo {
 public:
+  enum class reduction_operator { SUM, REPLACE, MIN, MAX };
   class group {
     friend class halo;
 
   public:
     /// Construct a halo segment from a vector of indices
     group(std::size_t rank, const std::vector<std::size_t> &indices)
-        : rank_(rank), indices_(indices), do_pack_(indices.size() > 1) {}
+        : rank_(rank), indices_(indices), do_pack_(indices.size() > 1),
+          op_(reduction_operator::REPLACE) {}
 
   private:
     std::size_t buffer_size() { return do_pack_ ? indices_.size() : 0; }
@@ -27,8 +29,24 @@ public:
     void unpack(T *data) {
       if (do_pack_) {
         auto b = buffer_;
-        for (auto &index : indices_)
-          data[index] = *b++;
+        switch (op_) {
+        case reduction_operator::REPLACE:
+          for (auto &index : indices_)
+            data[index] = *b++;
+          break;
+        case reduction_operator::SUM:
+          for (auto &index : indices_)
+            data[index] += *b++;
+          break;
+        case reduction_operator::MIN:
+          for (auto &index : indices_)
+            data[index] = std::min<T>(data[index], *b++);
+          break;
+        case reduction_operator::MAX:
+          for (auto &index : indices_)
+            data[index] = std::max<T>(data[index], *b++);
+          break;
+        }
       }
     }
 
@@ -42,10 +60,13 @@ public:
     }
 
     /// Asynchronous receive of group
-    void receive(communicator comm, T *data) {
+    void receive(communicator comm, T *data,
+                 reduction_operator op = reduction_operator::REPLACE) {
       if (do_pack_) {
+        op_ = op;
         comm.irecv(buffer_, indices_.size() * sizeof(T), rank_, request_);
       } else {
+        assert(op == reduction_operator::REPLACE);
         comm.irecv(&data[indices_[0]], sizeof(T), rank_, request_);
       }
     }
@@ -54,6 +75,7 @@ public:
     std::size_t rank_;
     std::vector<std::size_t> indices_;
     bool do_pack_;
+    reduction_operator op_;
     T *buffer_ = nullptr;
     MPI_Request *request_ = nullptr;
   };
@@ -71,64 +93,92 @@ public:
   }
 
   /// Construct a halo from groups
-  halo(communicator comm, T *data, const groups &sends, const groups &receives)
-      : comm_(comm), data_(data), receives_(receives), sends_(sends) {
+  halo(communicator comm, T *data, const groups &owned, const groups &halos)
+      : comm_(comm), data_(data), halos_(halos), owned_(owned),
+        pending_exchange_(false), pending_reduction_(false) {
 
     // Compute size of buffer
     std::size_t buffer_size = 0;
-    for (auto &r : receives_) {
+    for (auto &r : halos_) {
       buffer_size += r.buffer_size();
     }
-    for (auto &r : sends_) {
+    for (auto &r : owned_) {
       buffer_size += r.buffer_size();
     }
 
     buffer_.resize(buffer_size);
-    requests_.resize(sends.size() + receives.size());
+    requests_.resize(owned_.size() + halos_.size());
 
     auto bp = buffer_.data();
     auto rp = requests_.data();
-    allocate_storage(receives_, bp, rp);
-    allocate_storage(sends_, bp, rp);
+    allocate_storage(halos_, bp, rp);
+    allocate_storage(owned_, bp, rp);
   }
 
   /// Asynchronous halo exchange
   void exchange() {
+    assert(pending_reduction_ == false);
     // Post the receives
-    for (auto &receive : receives_) {
+    for (auto &receive : halos_) {
       receive.receive(comm_, data_);
     }
 
     // Send the data
-    for (auto &send : sends_) {
+    for (auto &send : owned_) {
       send.pack(data_);
       send.send(comm_, data_);
     }
+    pending_exchange_ = true;
+  }
+
+  /// Asynchronous halo reduction
+  void reduce(reduction_operator op = reduction_operator::SUM) {
+    assert(pending_exchange_ == false);
+    // Post the receives
+    for (auto &receive : owned_) {
+      receive.receive(comm_, data_, op);
+    }
+
+    // Send the data
+    for (auto &send : halos_) {
+      send.pack(data_);
+      send.send(comm_, data_);
+    }
+    pending_reduction_ = true;
   }
 
   /// Wait for halo exchange to complete
   void wait() {
     // below assumes receives are first in requests array
-    assert(sends_.size() == 0 || receives_.size() == 0 ||
-           sends_[0].request_ > receives_[0].request_);
+    assert(owned_.size() == 0 || halos_.size() == 0 ||
+           owned_[0].request_ > halos_[0].request_);
+    assert(pending_exchange_ || pending_reduction_);
+
     int pending = requests_.size();
 
     while (pending > 0) {
       int completed;
       MPI_Waitany(requests_.size(), requests_.data(), &completed,
                   MPI_STATUS_IGNORE);
-      if (std::size_t(completed) < receives_.size()) {
-        receives_[completed].unpack(data_);
+      if (pending_exchange_ && std::size_t(completed) < halos_.size()) {
+        halos_[completed].unpack(data_);
+      }
+      if (pending_reduction_ && std::size_t(completed) >= halos_.size()) {
+        owned_[completed - halos_.size()].unpack(data_);
       }
       pending--;
     }
+    pending_exchange_ = false;
+    pending_reduction_ = false;
   }
 
 private:
   communicator comm_;
   T *data_;
-  groups receives_;
-  groups sends_;
+  groups halos_;
+  groups owned_;
+  bool pending_exchange_;
+  bool pending_reduction_;
   std::vector<T> buffer_;
   std::vector<MPI_Request> requests_;
 };
--- tests/cpu/halo.cpp
+++ tests/cpu/halo.cpp
@@ -16,19 +16,121 @@ TEST(CpuMpiTests, Halo) {
   auto right = (comm_rank + 1) % comm_size;
   auto left = (comm_rank + comm_size - 1) % comm_size;
 
-  halo::group send_clockwise(right, {n - 1 - radius});
-  halo::group receive_clockwise(left, {0});
+  halo::group owned_clockwise(right, {n - 1 - radius});
+  halo::group owned_counter_clockwise(left, {radius});
 
-  halo::group send_counter_clockwise(left, {radius});
-  halo::group receive_counter_clockwise(right, {n - radius});
+  halo::group halos_clockwise(left, {0});
+  halo::group halos_counter_clockwise(right, {n - radius});
 
   // pointer
-  halo h(comm, d.data(), {send_clockwise, send_counter_clockwise},
-         {receive_clockwise, receive_counter_clockwise});
+  halo h(comm, d.data(), {owned_clockwise, owned_counter_clockwise},
+         {halos_clockwise, halos_counter_clockwise});
 
   h.exchange();
   h.wait();
 
   EXPECT_EQ(d[0], initial_value(left) + n - 2 * radius - 1);
   EXPECT_EQ(d[n - 1], initial_value(right));
+}
+
+TEST(CpuMpiTests, UnstructuredHalo) {
+
+  std::vector<int> d(n);
+  std::iota(d.begin(), d.end(), initial_value(comm_rank));
+
+  if (comm_size != 2) {
+    return;
+  }
+
+  std::vector<halo::group> owned, halos;
+  if (comm_rank == 0) {
+    owned.emplace_back(halo::group(1, {2, 2}));
+    halos.emplace_back(halo::group(1, {7, 8, 9}));
+  } else {
+    owned.emplace_back(halo::group(0, {1, 3, 5}));
+    halos.emplace_back(halo::group(0, {8, 9}));
+  }
+
+  halo h(comm, d.data(), owned, halos);
+
+  h.exchange();
+  h.wait();
+
+  if (comm_rank == 0) {
+    std::vector<int> correct = {100, 101, 102, 103, 104,
+                                105, 106, 201, 203, 205};
+    expect_eq(d, correct);
+  } else {
+    std::vector<int> correct = {200, 201, 202, 203, 204,
+                                205, 206, 207, 102, 102};
+    expect_eq(d, correct);
+  }
+}
+
+TEST(CpuMpiTests, UnstructuredHaloReduce) {
+
+  std::vector<int> d(n);
+  std::iota(d.begin(), d.end(), initial_value(comm_rank));
+
+  if (comm_size != 2) {
+    return;
+  }
+
+  std::vector<halo::group> sends, receives;
+  if (comm_rank == 0) {
+    sends.emplace_back(halo::group(1, {2, 2}));
+    receives.emplace_back(halo::group(1, {7, 8, 9}));
+  } else {
+    sends.emplace_back(halo::group(0, {1, 3, 5}));
+    receives.emplace_back(halo::group(0, {8, 9}));
+  }
+
+  halo h(comm, d.data(), sends, receives);
+
+  h.reduce();
+  h.wait();
+
+  if (comm_rank == 0) {
+    std::vector<int> correct = {100, 101, 519, 103, 104,
+                                105, 106, 107, 108, 109};
+    expect_eq(d, correct);
+  } else {
+    std::vector<int> correct = {200, 308, 202, 311, 204,
+                                314, 206, 207, 208, 209};
+    expect_eq(d, correct);
+  }
+}
+
+TEST(CpuMpiTests, UnstructuredHaloReduceMax) {
+
+  std::vector<int> d(n);
+  std::iota(d.begin(), d.end(), initial_value(comm_rank));
+
+  if (comm_size != 2) {
+    return;
+  }
+
+  std::vector<halo::group> sends, receives;
+  if (comm_rank == 0) {
+    sends.emplace_back(halo::group(1, {2, 2}));
+    receives.emplace_back(halo::group(1, {7, 8, 9}));
+  } else {
+    sends.emplace_back(halo::group(0, {1, 3, 5}));
+    receives.emplace_back(halo::group(0, {8, 9}));
+  }
+
+  halo h(comm, d.data(), sends, receives);
+
+  h.reduce(halo::reduction_operator::MAX);
+  h.wait();
+
+  if (comm_rank == 0) {
+    std::vector<int> correct = {100, 101, 209, 103, 104,
+                                105, 106, 107, 108, 109};
+    expect_eq(d, correct);
+  } else {
+    std::vector<int> correct = {200, 201, 202, 203, 204,
+                                205, 206, 207, 208, 209};
+    expect_eq(d, correct);
+  }
 }
