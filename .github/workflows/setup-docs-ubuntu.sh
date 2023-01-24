3fd3ce9319d59e00db7112001bcca66d9ee0a867
Robert Cohn <robert.s.cohn@intel.com>
2023-01-24 17:43:27 -0600

    update

--- .github/workflows/setup-docs-ubuntu.sh
+++ .github/workflows/setup-docs-ubuntu.sh
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+curl -s https://www.doxygen.nl/files/doxygen-1.9.6.linux.bin.tar.gz -o /tmp/dox.tgz
+sudo tar zxf /tmp/dox.tgz -C /usr/local
+sudo ln -s /usr/local/doxgen*/bin/* /usr/local/bin
 sudo apt install -y \
-     graphviz       \
-     doxygen
+     graphviz
