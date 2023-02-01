e0a019ca499cb3476ced16dde276da7aaf37fbc9
Łukasz Ślusarczyk <112692748+lslusarczyk@users.noreply.github.com>
2023-02-01 15:19:44 +0100

    copy operations final cleanups (#11)
    * Implement copy_anysize
    
    Implement a collective copy without providing the size of the source in the destination.
    
    * added mapping from type to MPI_Datatype
    
    * fixed compilation with openmpi
    
    * renamed copy_anysize to copy, removed one copy implemention which takes two iterators and does not send size
    
    * renamed DI to I in one copy
    
    * unify 2 copy functions using constexpr and add some minor changes
    
    * modifications to follow spdm style
    
    * update nullptr into braces so it is excluded from spell check
    
    Co-authored-by: Robert Cohn <rscohn2@gmail.com>
    
    * add delimiters around code in comments of copy
    
    Co-authored-by: Robert Cohn <rscohn2@gmail.com>
    
    * changed MPI_CHAR to MPI_BYTE, removed nullptr from spelling
    
    * made bcast non-template again
    
    * fixed compilation
    
    * moved back func desc in copy.rst
    
    * made all copy functions consistent wrp to signatures, comments and SPMD style
    
    * fixed a typo
    
    * fixed formatting of comments
    
    ---------
    
    Co-authored-by: Kacper Stefanski <kacper.stefanski@intel.com>
    Co-authored-by: Robert Cohn <rscohn2@gmail.com>

--- test/gtest/cpu/algorithms.cpp
+++ test/gtest/cpu/algorithms.cpp
@@ -12,7 +12,7 @@ void check_copy(std::size_t n, std::size_t b, std::size_t e) {
   rng::iota(v_in, 100);
 
   lib::distributed_vector<int> dv_in(n), dv1(n), dv2(n), dv3(n), dv4(n), dv5(n),
-      dv6(n), dv7(n);
+      dv6(n), dv7(n), dv8(n), dv9(n);
   lib::iota(dv_in, 100);
   lib::copy(dv_in.begin() + b, dv_in.begin() + e, dv1.begin() + b);
   lib::copy(rng::subrange(dv_in.begin() + b, dv_in.begin() + e),
@@ -46,6 +46,15 @@ void check_copy(std::size_t n, std::size_t b, std::size_t e) {
   lib::copy(0, dv_in.begin() + b, dv_in.begin() + e,
             comm_rank == 0 ? &*(v3.begin() + b) : nullptr);
 
+  if (comm_rank == 0) {
+    lib::copy(0, v_in.begin() + b, v_in.begin() + e, dv8.begin() + b);
+  } else {
+    lib::copy(0, nullptr, nullptr, dv8.begin() + b);
+  }
+
+  lib::copy(0, comm_rank == 0 ? &*(v_in.begin() + b) : nullptr,
+            comm_rank == 0 ? &*(v_in.begin() + e) : nullptr, dv9.begin() + b);
+
   dv3.fence();
 
   if (comm_rank == 0) {
@@ -58,6 +67,8 @@ void check_copy(std::size_t n, std::size_t b, std::size_t e) {
     EXPECT_TRUE(equal(dv5, v));
     EXPECT_TRUE(equal(dv6, v));
     EXPECT_TRUE(equal(dv7, v));
+    EXPECT_TRUE(equal(dv8, v));
+    EXPECT_TRUE(equal(dv9, v));
 
     EXPECT_TRUE(equal(v1, v));
     EXPECT_TRUE(equal(v2, v));
