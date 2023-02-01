ec1ec19784661ec7603bc0843cf08aebf04df6bf
Robert Cohn <robert.s.cohn@intel.com>
2023-02-01 16:07:04 -0600

    zip iter to segments working without cpo

--- test/gtest/mhp/views.cpp
+++ test/gtest/mhp/views.cpp
@@ -20,6 +20,54 @@ TEST(MhpTests, Subrange) {
   static_assert(lib::distributed_range<decltype(r)>);
 }
 
+//
+// Zip the segments for 1 or more distributed ranges. e.g.:
+//
+//   segments(dv1): [[10, 11, 12, 13, 14], [15, 16, 17, 18, 19]]
+//   segments(dv2): [[20, 21, 22, 23, 24], [25, 26, 27, 28, 29]]
+//
+//   drop the first 4 elements and zip the segments for the rest
+//
+//    zip segments: [[(14, 24)], [(15, 25), (16, 26), (17, 27), (18, 28), (19,
+//    29)]]
+//
+template <typename... Ss> auto zip_segments(Ss &&...iters) {
+  auto zip_segment = [](auto &&v) {
+    auto zip = [](auto &&...refs) { return rng::views::zip(refs...); };
+    return std::apply(zip, v);
+  };
+
+  return rng::views::zip(lib::ranges::segments(iters)...) |
+         rng::views::transform(zip_segment);
+}
+
+//
+// Given an iter for a zip, return the segmentation
+//
+auto zip_iter_segments(auto zip_iter) {
+  // Given the list of refs as arguments, convert to list of iters
+  auto zip = [](auto &&...refs) { return zip_segments(&refs...); };
+
+  // Convert the zip iterator to a tuple of references, and pass the
+  // references as a list of arguments
+  return std::apply(zip, *zip_iter);
+}
+
+TEST(MhpTests, Zip) {
+  DV dv1(10), dv2(10);
+  mhp::iota(dv1, 10);
+  mhp::iota(dv2, 20);
+  auto dzv = rng::view::zip(dv1, dv2);
+  fmt::print("dzv: {}\n"
+             "  dv1: {}\n"
+             "  dv2: {}\n"
+             "  segments(dv1): {}\n"
+             "  segments(dv2): {}\n",
+             dzv, dv1, dv2, lib::ranges::segments(dv1),
+             lib::ranges::segments(dv2));
+  fmt::print("zip segments: {}\n", zip_iter_segments(dzv.begin() + 4));
+}
+
 TEST(MhpTests, Take) {
   const int n = 10;
   V a(n);
