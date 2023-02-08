1610917138cfb377ebdc4897c7ba117096a74dbd
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-02-08 15:01:22 +0100

    shp::copy_async() local -> distributed

--- include/dr/shp/copy.hpp
+++ include/dr/shp/copy.hpp
@@ -105,6 +105,48 @@ OutputIt copy(InputIt first, InputIt last, OutputIt d_first) {
   return rv;
 }
 
+template <std::forward_iterator InputIt, lib::distributed_iterator OutputIt>
+cl::sycl::event copy_async(InputIt first, InputIt last, OutputIt d_first) {
+  auto segments = lib::ranges::segments(d_first);
+
+  std::size_t segment_id = lib::ranges::segment_index(d_first);
+  std::size_t local_id = lib::ranges::local_index(d_first);
+
+  std::vector<cl::sycl::event> events;
+
+  std::size_t total_copied = 0;
+
+  while (first != last) {
+    auto &&segment = segments[segment_id];
+    std::size_t n_in_segment = segment.size() - local_id;
+
+    std::size_t n_to_copy =
+        std::min<size_t>(n_in_segment, std::distance(first, last));
+
+    auto local_last = first;
+    std::advance(local_last, n_to_copy);
+
+    auto remote_iter = segment.begin();
+    std::advance(remote_iter, local_id);
+
+    auto event = shp::copy_async(first, local_last, remote_iter);
+
+    events.push_back(event);
+
+    local_id = 0;
+    segment_id++;
+    std::advance(first, n_to_copy);
+    total_copied += n_to_copy;
+  }
+
+  sycl::queue q;
+
+  auto root_event = q.submit([=](auto &&h) { h.depends_on(events); });
+
+  return root_event;
+}
+
+
 // Copy from distributed range to local range
 template <lib::distributed_iterator InputIt, std::forward_iterator OutputIt>
 cl::sycl::event copy_async(InputIt first, InputIt last, OutputIt d_first) {
