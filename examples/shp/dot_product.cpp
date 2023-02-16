28053433273dc7e9a9d08ff29e3b66625db86fa9
Benjamin Brock <benjamin.brock@intel.com>
2023-02-16 10:53:07 -0800

    Implement `dot_product` example

---
+++ examples/shp/dot_product.cpp
@@ -0,0 +1,37 @@
+#include <dr/shp/shp.hpp>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+#include <ranges>
+
+template <lib::distributed_range X,
+          lib::distributed_range Y>
+auto dot_product(X&& x, Y&& y) {
+  auto z =   shp::views::zip(x, y)
+           | shp::views::transform(
+               [](auto&& elem) {
+                 auto&& [a, b] = elem;
+                 return a * b;
+               });
+
+  return shp::reduce(shp::par_unseq, z, 0, std::plus());
+}
+
+int main(int argc, char** argv) {
+  namespace sycl = cl::sycl;
+  auto devices = shp::get_numa_devices(sycl::gpu_selector_v);
+  shp::init(devices);
+
+  std::size_t n = 100;
+
+  shp::distributed_vector<int> x(n);
+  shp::distributed_vector<int> y(n);
+
+  std::iota(x.begin(), x.end(), 0);
+  std::iota(y.begin(), y.end(), 0);
+
+  auto v = dot_product(x, y);
+
+  fmt::print("{}\n", v);
+
+  return 0;
+}
