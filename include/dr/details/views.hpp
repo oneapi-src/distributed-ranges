6714fd68b1401d304d899637ac75ba0cf14167b6
Robert Cohn <robert.s.cohn@intel.com>
2023-02-16 09:44:58 -0600

    more cleanup

--- include/dr/details/views.hpp
+++ include/dr/details/views.hpp
@@ -4,36 +4,6 @@
 
 namespace lib {
 
-template <typename R> class view_local_span : public rng::view_base {
-  using distributed_container_type =
-      typename rng::iterator_t<R>::container_type;
-  using local_container_type = typename distributed_container_type::local_type;
-  using local_iterator = typename rng::iterator_t<local_container_type>;
-
-public:
-  /// Constructor
-  view_local_span(R &&r) : begin_(r.begin().local()), end_(r.end().local()) {}
-
-  local_iterator begin() { return begin_; }
-  local_iterator end() { return end_; }
-
-private:
-  local_iterator begin_;
-  local_iterator end_;
-};
-
-template <typename R> view_local_span(R &&) -> view_local_span<R>;
-
-struct local_span {
-  template <typename R> auto operator()(R &&r) const {
-    return view_local_span(std::forward<R>(r));
-  }
-
-  template <typename R> friend auto operator|(R &&r, const local_span &) {
-    return view_local_span(std::forward<R>(r));
-  }
-};
-
 // returns range: [(rank, element) ...]
 auto ranked_view(const lib::distributed_range auto &r) {
   auto rank = [](auto &&v) { return lib::ranges::rank(&v); };
