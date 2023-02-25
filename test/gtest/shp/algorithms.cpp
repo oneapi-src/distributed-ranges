b32344a3d37a2997ca56d6e4280daef68cf2f489
Benjamin Brock <benjamin.brock@intel.com>
2023-02-24 18:30:35 -0800

    Update to test all versions of `inclusive_scan`

--- test/gtest/shp/algorithms.cpp
+++ test/gtest/shp/algorithms.cpp
@@ -62,8 +62,10 @@ TEST(ShpTests, InclusiveScan) {
   std::size_t n = 100;
 
   shp::distributed_vector<int, shp::device_allocator<int>> v(n);
+  shp::distributed_vector<int, shp::device_allocator<int>> o(v.size() * 2);
   std::vector<int> lv(n);
 
+  // Range case, no binary op or init, perfectly aligned
   for (auto &&x : lv) {
     x = lrand48() % 100;
   }
@@ -76,15 +78,70 @@ TEST(ShpTests, InclusiveScan) {
     EXPECT_EQ(lv[i], v[i]);
   }
 
+  // Range case, binary op no init, non-aligned ranges
   for (auto &&x : lv) {
     x = lrand48() % 100;
   }
   shp::copy(lv.begin(), lv.end(), v.begin());
 
-  shp::distributed_vector<int, shp::device_allocator<int>> o(v.size() * 2);
+  std::inclusive_scan(lv.begin(), lv.end(), lv.begin());
+  shp::inclusive_scan(shp::par_unseq, v, o, std::plus<>());
+
+  for (size_t i = 0; i < lv.size(); i++) {
+    EXPECT_EQ(lv[i], o[i]);
+  }
+
+  // Range case, binary op, init, non-aligned ranges
+  for (auto &&x : lv) {
+    x = lrand48() % 100;
+  }
+  shp::copy(lv.begin(), lv.end(), v.begin());
+
+  std::inclusive_scan(lv.begin(), lv.end(), lv.begin(), std::multiplies<>(),
+                      12);
+  shp::inclusive_scan(shp::par_unseq, v, o, std::multiplies<>(), 12);
+
+  for (size_t i = 0; i < lv.size(); i++) {
+    EXPECT_EQ(lv[i], o[i]);
+  }
+
+  // Iterator case, no binary op or init, perfectly aligned
+  for (auto &&x : lv) {
+    x = lrand48() % 100;
+  }
+  shp::copy(lv.begin(), lv.end(), v.begin());
 
   std::inclusive_scan(lv.begin(), lv.end(), lv.begin());
-  shp::inclusive_scan(shp::par_unseq, v, o);
+  shp::inclusive_scan(shp::par_unseq, v.begin(), v.end(), v.begin());
+
+  for (size_t i = 0; i < lv.size(); i++) {
+    EXPECT_EQ(lv[i], v[i]);
+  }
+
+  // Iterator case, binary op no init, non-aligned ranges
+  for (auto &&x : lv) {
+    x = lrand48() % 100;
+  }
+  shp::copy(lv.begin(), lv.end(), v.begin());
+
+  std::inclusive_scan(lv.begin(), lv.end(), lv.begin());
+  shp::inclusive_scan(shp::par_unseq, v.begin(), v.end(), o.begin(),
+                      std::plus<>());
+
+  for (size_t i = 0; i < lv.size(); i++) {
+    EXPECT_EQ(lv[i], o[i]);
+  }
+
+  // Range case, binary op, init, non-aligned ranges
+  for (auto &&x : lv) {
+    x = lrand48() % 100;
+  }
+  shp::copy(lv.begin(), lv.end(), v.begin());
+
+  std::inclusive_scan(lv.begin(), lv.end(), lv.begin(), std::multiplies<>(),
+                      12);
+  shp::inclusive_scan(shp::par_unseq, v.begin(), v.end(), o.begin(),
+                      std::multiplies<>(), 12);
 
   for (size_t i = 0; i < lv.size(); i++) {
     EXPECT_EQ(lv[i], o[i]);
