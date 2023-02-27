fbc0d4eb1af4b6892731b572fd57d2ee0973d1ab
Robert Cohn <robert.s.cohn@intel.com>
2023-02-27 14:43:19 -0500

    mhp-sycl for_each (#59)

--- include/dr/mhp/sycl_support.hpp
+++ include/dr/mhp/sycl_support.hpp
@@ -4,11 +4,13 @@
 
 namespace mhp {
 
+#ifdef SYCL_LANGUAGE_VERSION
 namespace _detail {
 
 template <typename T, std::size_t Alignment>
 using shared_base_allocator =
     sycl::usm_allocator<T, sycl::usm::alloc::shared, Alignment>;
+
 }; // namespace _detail
 
 template <typename T, std::size_t Alignment = 0>
@@ -18,5 +20,18 @@ public:
   sycl_shared_allocator(sycl::queue q = sycl_queue())
       : _detail::shared_base_allocator<T, Alignment>(q) {}
 };
+
+struct device_policy {
+  device_policy(sycl::queue q = sycl_queue()) : queue(q), dpl_policy(q) {}
+
+  sycl::queue queue;
+  decltype(oneapi::dpl::execution::make_device_policy(queue)) dpl_policy;
+};
+
+#else // !SYCL_LANGUAGE_VERSION
+
+struct device_policy {};
+
+#endif // SYCL_LANGUAGE_VERSION
 
 } // namespace mhp
