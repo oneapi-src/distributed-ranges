88b4cfb64b46ac91fcb914cc8f572b98f94837bd
Łukasz Ślusarczyk <lukasz.slusarczyk@intel.com>
2023-03-09 11:57:44 +0100

    added copyright note

--- test/gtest/shp/fill.cpp
+++ test/gtest/shp/fill.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
 #include "shp-tests.hpp"
 
 template <typename AllocT> class FillTest : public testing::Test {
