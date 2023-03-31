0f612c9e8af320bb7de4428728800a719dd63132
Robert Cohn <robert.s.cohn@intel.com>
2023-03-31 13:35:05 -0400

    mhp zip iota support (#160)

--- include/dr/mhp/alignment.hpp
+++ include/dr/mhp/alignment.hpp
@@ -2,29 +2,45 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+#pragma once
+
+#include <dr/concepts/concepts.hpp>
+#include <dr/details/ranges.hpp>
+#include <dr/details/ranges_shim.hpp>
+
 namespace mhp {
 
-// It won't have segments if it is a zip of non-aligned data
-bool aligned(lib::distributed_iterator auto iter) {
-  return !lib::ranges::segments(iter).empty();
-}
+auto aligned(auto &&r) { return !lib::ranges::segments(r).empty(); }
 
 // iter1 is aligned with iter2, and iter2 is aligned with the rest
 bool aligned(lib::distributed_iterator auto iter1,
              lib::distributed_iterator auto iter2,
              lib::distributed_iterator auto... iters) {
-  auto combined = rng::views::zip(lib::ranges::segments(iter1),
-                                  lib::ranges::segments(iter2));
-  if (combined.empty())
-    return false;
-  for (auto seg : combined) {
-    if (lib::ranges::rank(seg.first) != lib::ranges::rank(seg.second) ||
-        rng::distance(seg.first) != rng::distance(seg.second)) {
+  for (auto seg : rng::views::zip(lib::ranges::segments(iter1),
+                                  lib::ranges::segments(iter2))) {
+    if (lib::ranges::rank(seg.first) != lib::ranges::rank(seg.second)) {
+      return false;
+    }
+    if (rng::distance(seg.first) != rng::distance(seg.second)) {
       return false;
     }
   }
 
   return aligned(iter2, iters...);
+}
+
+template <typename T>
+concept local_iterator = !lib::distributed_iterator<T>;
+
+// Skip local iterators
+bool aligned(local_iterator auto iter1, lib::distributed_iterator auto iter2,
+             lib::distributed_iterator auto... iters) {
+  return aligned(iter2, iters...);
+}
+
+bool aligned(lib::distributed_iterator auto iter1, local_iterator auto iter2,
+             lib::distributed_iterator auto... iters) {
+  return aligned(iter1, iters...);
 }
 
 } // namespace mhp
--- include/dr/mhp/views/zip.hpp
+++ include/dr/mhp/views/zip.hpp
@@ -2,7 +2,63 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+#pragma once
+
+#include <cstddef>
+#include <iterator>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#include <dr/details/ranges_shim.hpp>
+#include <dr/mhp/alignment.hpp>
+
 namespace mhp {
+
+inline std::size_t select_rank_from_iter(auto iter, auto... rest) {
+  if constexpr (lib::distributed_iterator<decltype(iter)>) {
+    return lib::ranges::rank(iter);
+  } else {
+    return select_rank_from_iter(rest...);
+  }
+}
+
+inline std::size_t zip_segment_rank(auto &&segment) {
+  auto select_rank_from_ref = [](auto &&...refs) {
+    return select_rank_from_iter((&refs)...);
+  };
+  return std::apply(select_rank_from_ref, segment[0]);
+}
+
+template <typename T>
+concept distributed_reference = requires(T &t) { lib::ranges::rank(&t); };
+
+template <typename T>
+concept tuple_like = requires(T &t) { std::get<0>(t); };
+
+template <typename T>
+concept tuple_has_distributed_reference =
+    []<std::size_t... N>(std::index_sequence<N...>) {
+      return (distributed_reference<typename std::tuple_element<N, T>::type> ||
+              ...);
+    }(std::make_index_sequence<std::tuple_size_v<T>>());
+
+template <typename ZR>
+concept zip_segment =
+    rng::forward_range<ZR>
+    // value is a tuple
+    && tuple_like<rng::range_reference_t<ZR>>
+    // at least 1 member of the tuple is a distributed reference
+    && tuple_has_distributed_reference<rng::range_reference_t<ZR>>;
+
+inline auto select_dist_range(auto &&v, auto &&...rest) {
+  if constexpr (lib::distributed_range<decltype(v)>) {
+    return rng::views::all(v);
+  } else {
+    return select_dist_range(rest...);
+  }
+}
 
 template <typename I>
 concept is_zip_iterator =
@@ -13,8 +69,16 @@ class zip_view : public rng::view_interface<zip_view<R...>> {
 public:
   zip_view(const zip_view &z) : base_(z.base_) {}
   zip_view(zip_view &&z) : base_(std::move(z.base_)) {}
-  template <typename... V>
-  zip_view(V &&...v) : base_(rng::views::all(std::forward<V>(v))...) {}
+
+  template <typename... V> zip_view(V &&...v) : base_(rng::views::all(v)...) {
+    auto segments = lib::ranges::segments(select_dist_range(v...));
+    segment_descriptor descriptor{0, 0};
+    for (auto segment : segments) {
+      descriptor.size = rng::distance(segment);
+      segment_descriptors_.emplace_back(descriptor);
+      descriptor.offset += descriptor.size;
+    }
+  }
 
   auto begin() const {
     return lib::normal_distributed_iterator<decltype(segments())>(
@@ -28,13 +92,13 @@ public:
   }
 
   auto segments() const {
-    auto zip_segments = [](auto &&...base) {
+    auto zip_segments = [this](auto &&...base) {
       auto zip_segment = [](auto &&v) {
         auto zip = [](auto &&...refs) { return rng::views::zip(refs...); };
         return std::apply(zip, v);
       };
 
-      auto z = rng::views::zip(lib::ranges::segments(base)...) |
+      auto z = rng::views::zip(this->base_segments(base)...) |
                rng::views::transform(zip_segment);
       if (aligned(rng::begin(base)...)) {
         return z;
@@ -49,7 +113,29 @@ public:
   auto base() const { return base_; }
 
 private:
+  struct segment_descriptor {
+    std::size_t offset, size;
+  };
+
+  template <rng::range V>
+    requires(lib::is_iota_view_v<std::remove_cvref_t<V>>)
+  auto base_segments(V &&base) const {
+    auto make_iota_segment = [base](const segment_descriptor &d) {
+      return rng::subrange(rng::begin(base) + d.offset,
+                           rng::begin(base) + d.offset + d.size);
+    };
+
+    return segment_descriptors_ | rng::views::transform(make_iota_segment);
+  }
+
+  template <rng::range V> auto base_segments(V &&base) const {
+    return lib::ranges::segments(base);
+    ;
+  }
+
   std::tuple<R...> base_;
+  // For large number of segments, this will be expensive to copy
+  std::vector<segment_descriptor> segment_descriptors_;
 };
 
 template <rng::viewable_range... R>
@@ -76,5 +162,7 @@ template <mhp::is_zip_iterator ZI> auto local_(ZI zi) {
   };
   return std::apply(refs_to_local_zip_iterator, *zi);
 }
+
+template <mhp::zip_segment V> auto rank_(V &&v) { return zip_segment_rank(v); }
 
 } // namespace DR_RANGES_NAMESPACE
