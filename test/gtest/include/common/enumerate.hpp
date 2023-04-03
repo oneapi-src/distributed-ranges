6940ae7e4c28a65d76f1caf27ffc1f07b3487c52
Suyash Bakshi <suyash.bakshi@intel.com>
2023-04-03 12:31:31 -0700

    cleanup test/gtest/include/common/enumerate.hpp

--- test/gtest/include/common/enumerate.hpp
+++ test/gtest/include/common/enumerate.hpp
@@ -12,15 +12,6 @@ TYPED_TEST_SUITE(Enumerate, AllTypes);
 TYPED_TEST(Enumerate, Basic) {
   Ops1<TypeParam> ops(10);
 
-  // std::vector<int> v(5);
-  // v[0] = 5;
-  // v[1] = 5;
-  // v[2] = 5;
-  // v[3] = 5;
-  // v[4] = 5;
-
-  // EXPECT_TRUE(
-  //     check_view(rng::views::all(v), rng::views::all(ops.dist_vec)));
   EXPECT_TRUE(
       check_view(shp::views::enumerate(ops.vec), shp::views::enumerate(ops.dist_vec)));
 }
