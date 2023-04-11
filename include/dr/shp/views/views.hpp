f1ab28313a16ae924d605cfdeab4936c1a0ac18d
Benjamin Brock <benjamin.brock@intel.com>
2023-04-10 17:16:55 -0700

    Add `shp::views::transform` CPO to `shp`

---
+++ include/dr/shp/views/views.hpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <dr/views/views.hpp>
+
+namespace shp {
+
+namespace views {
+
+inline constexpr auto transform = lib::views::transform;
+
+} // namespace views
+
+} // namespace shp
