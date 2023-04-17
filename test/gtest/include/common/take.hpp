d9a0899f04f360ec5cebb8df553e877efd1a1002
Łukasz Ślusarczyk <lukasz.slusarczyk@intel.com>
2023-04-17 15:11:01 +0200

    fixed namespaces in take test

--- test/gtest/include/common/take.hpp
+++ test/gtest/include/common/take.hpp
@@ -82,13 +82,13 @@ TYPED_TEST(Take, large) {
 TYPED_TEST(Take, takeOfOneElementHasOneSegmentAndSameRank) {
   Ops1<TypeParam> ops(10);
   auto dist_rng_view = rng::views::take(ops.dist_vec, 1);
-  auto dist_xhp_view = shp::views::take(ops.dist_vec, 1);
+  auto dist_xhp_view = xhp::views::take(ops.dist_vec, 1);
 
-  auto dist_rng_segments = lib::ranges::segments(dist_rng_view);
-  auto dist_rng_rank = lib::ranges::rank(dist_rng_segments[0]);
+  auto dist_rng_segments = dr::ranges::segments(dist_rng_view);
+  auto dist_rng_rank = dr::ranges::rank(dist_rng_segments[0]);
 
-  auto dist_xhp_segments = lib::ranges::segments(dist_xhp_view);
-  auto dist_xhp_rank = lib::ranges::rank(dist_xhp_segments[0]);
+  auto dist_xhp_segments = dr::ranges::segments(dist_xhp_view);
+  auto dist_xhp_rank = dr::ranges::rank(dist_xhp_segments[0]);
 
   EXPECT_EQ(dist_rng_segments.size(), 1);
   EXPECT_EQ(dist_xhp_segments.size(), 1);
@@ -97,16 +97,16 @@ TYPED_TEST(Take, takeOfOneElementHasOneSegmentAndSameRank) {
 
 TYPED_TEST(Take, takeOfFirstSegementHasOneSegmentAndSameRank) {
   Ops1<TypeParam> ops(10);
-  auto dist_vec_seg = lib::ranges::segments(ops.dist_vec);
+  auto dist_vec_seg = dr::ranges::segments(ops.dist_vec);
   auto first_seg_size = dist_vec_seg[0].size();
 
   auto dist_rng_view = rng::views::take(ops.dist_vec, first_seg_size);
-  auto dist_rng_segments = lib::ranges::segments(dist_rng_view);
-  auto rng_segment_rank = lib::ranges::rank(dist_rng_segments[0]);
+  auto dist_rng_segments = dr::ranges::segments(dist_rng_view);
+  auto rng_segment_rank = dr::ranges::rank(dist_rng_segments[0]);
 
-  auto dist_xhp_view = shp::views::take(ops.dist_vec, first_seg_size);
-  auto dist_xhp_segments = lib::ranges::segments(dist_xhp_view);
-  auto xhp_segment_rank = lib::ranges::rank(dist_xhp_segments[0]);
+  auto dist_xhp_view = xhp::views::take(ops.dist_vec, first_seg_size);
+  auto dist_xhp_segments = dr::ranges::segments(dist_xhp_view);
+  auto xhp_segment_rank = dr::ranges::rank(dist_xhp_segments[0]);
 
   EXPECT_EQ(dist_rng_segments.size(), 1);
   EXPECT_EQ(dist_xhp_segments.size(), 1);
@@ -116,13 +116,13 @@ TYPED_TEST(Take, takeOfFirstSegementHasOneSegmentAndSameRank) {
 TYPED_TEST(Take, takeOfAllButOneSizeHasAllSegmentsWithSameRanks) {
   Ops1<TypeParam> ops(10);
 
-  auto dist_vec_seg = lib::ranges::segments(ops.dist_vec);
+  auto dist_vec_seg = dr::ranges::segments(ops.dist_vec);
 
   auto dist_rng_view = rng::views::take(ops.dist_vec, 9);
-  auto dist_xhp_view = shp::views::take(ops.dist_vec, 9);
+  auto dist_xhp_view = xhp::views::take(ops.dist_vec, 9);
 
-  auto dist_rng_segments = lib::ranges::segments(dist_rng_view);
-  auto dist_xhp_segments = lib::ranges::segments(dist_xhp_view);
+  auto dist_rng_segments = dr::ranges::segments(dist_rng_view);
+  auto dist_xhp_segments = dr::ranges::segments(dist_xhp_view);
 
   EXPECT_EQ(dist_vec_seg.size(), dist_rng_segments.size());
   EXPECT_EQ(dist_vec_seg.size(), dist_xhp_segments.size());
@@ -130,8 +130,8 @@ TYPED_TEST(Take, takeOfAllButOneSizeHasAllSegmentsWithSameRanks) {
   auto segments_size = dist_rng_segments.size();
 
   for (auto i = 0; i <= segments_size; i++) {
-    auto rng_segment_rank = lib::ranges::rank(dist_rng_segments[i]);
-    auto xhp_segment_rank = lib::ranges::rank(dist_xhp_segments[i]);
+    auto rng_segment_rank = dr::ranges::rank(dist_rng_segments[i]);
+    auto xhp_segment_rank = dr::ranges::rank(dist_xhp_segments[i]);
     EXPECT_EQ(rng_segment_rank, xhp_segment_rank);
   }
 }
@@ -139,13 +139,13 @@ TYPED_TEST(Take, takeOfAllButOneSizeHasAllSegmentsWithSameRanks) {
 TYPED_TEST(Take, takeOfMoreSizeHasSameNumberOfSegmentsAndSameRanks) {
   Ops1<TypeParam> ops(10);
 
-  auto dist_vec_seg = lib::ranges::segments(ops.dist_vec);
+  auto dist_vec_seg = dr::ranges::segments(ops.dist_vec);
 
   auto dist_rng_view = rng::views::take(ops.dist_vec, 11);
-  auto dist_xhp_view = shp::views::take(ops.dist_vec, 11);
+  auto dist_xhp_view = xhp::views::take(ops.dist_vec, 11);
 
-  auto dist_rng_segments = lib::ranges::segments(dist_rng_view);
-  auto dist_xhp_segments = lib::ranges::segments(dist_xhp_view);
+  auto dist_rng_segments = dr::ranges::segments(dist_rng_view);
+  auto dist_xhp_segments = dr::ranges::segments(dist_xhp_view);
 
   EXPECT_EQ(dist_vec_seg.size(), dist_rng_segments.size());
   EXPECT_EQ(dist_vec_seg.size(), dist_xhp_segments.size());
@@ -153,8 +153,8 @@ TYPED_TEST(Take, takeOfMoreSizeHasSameNumberOfSegmentsAndSameRanks) {
   auto segments_size = dist_rng_segments.size();
 
   for (auto i = 0; i <= segments_size; i++) {
-    auto rng_segment_rank = lib::ranges::rank(dist_rng_segments[i]);
-    auto xhp_segment_rank = lib::ranges::rank(dist_xhp_segments[i]);
+    auto rng_segment_rank = dr::ranges::rank(dist_rng_segments[i]);
+    auto xhp_segment_rank = dr::ranges::rank(dist_xhp_segments[i]);
     EXPECT_EQ(rng_segment_rank, xhp_segment_rank);
   }
 }
