f5e0d1c1f3cc3fc4c0da4236148cb405d8a22232
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-04-27 18:30:49 +0200

    tests for matrix

---
+++ test/gtest/mhp/distributed_dense_matrix.cpp
@@ -0,0 +1,89 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "xhp-tests.hpp"
+
+using T = int;
+using DM = dr::mhp::distributed_dense_matrix<T>;
+using A = std::allocator<T>;
+using DMA = dr::mhp::distributed_dense_matrix<T, A>;
+using DMI = typename DM::iterator;
+
+TEST(MhpDmTests, DM_Create) {
+  const int rows = 11, cols = 11;
+  DM a(rows, cols);
+
+  EXPECT_EQ(a.size(), rows * cols);
+}
+
+TEST(MhpDmTests, DM_CreateFill) {
+  const int rows = 11, cols = 11;
+  DM a(rows, cols, -1);
+
+  EXPECT_EQ(*(a.begin()), -1);
+  EXPECT_EQ(*(a.begin() + 13), -1);
+  EXPECT_EQ(*(a.end()-1), -1);
+}
+
+// TODO: index operator
+
+/* TEST(MhpDmTests, DistributedMatrix_Index) {
+  const int rows = 11, cols = 11;
+  DM a(rows, cols, -1);
+
+  EXPECT_EQ(a.begin()[55], -1);
+  EXPECT_EQ(a[{4,4}], -1);
+
+} */
+
+/* TEST(MhpTestsDM, DM_For) {
+  const int rows = 11, cols = 11;
+  DM a(rows, cols, -1);
+
+  for(auto i : a) {
+    i = 5;
+  }
+
+  EXPECT_EQ(a.begin()[0], 5);
+  EXPECT_EQ(a.begin()[55], 5);
+  EXPECT_EQ(a.begin()[rows * cols - 1], 5);
+
+} */
+
+/* 
+TEST(MhpTestsDM, DM_Fill) {
+  const int rows = 11, cols = 11;
+  DM a(rows, cols, -13);
+
+  dr::mhp::fill(a, 5);
+
+  if (comm_rank == 0) 
+    EXPECT_EQ(a.begin()[0], 5);
+  if (comm_rank == comm_size - 1) 
+    EXPECT_EQ(a.begin()[rows * cols - 1], 5);
+
+}
+
+TEST(MhpTestsDM, DM_Fill_HB) {
+  const int rows = 11, cols = 11;
+  dr::halo_bounds hb(1);
+  DM a(rows, cols, -13, hb);
+  
+  dr::mhp::fill(a, 5);
+  a.dump_matrix("A1");
+
+  if (comm_rank == 0) {
+    fmt::print("{}: check after fill {} == 5, {} == 5\n", comm_rank, a.begin()[0], a.begin()[120]);
+    EXPECT_EQ(a.begin()[0], 5);
+    EXPECT_EQ(a.begin()[65], 5);
+  }
+  if (comm_rank == comm_size - 1) 
+  {
+    fmt::print("{}: check after fill {} == 5, {} == 5\n", comm_rank, a.begin()[65], a.begin()[120]);
+    EXPECT_EQ(a.begin()[65], 5);
+    EXPECT_EQ(a.begin()[120], 5);
+  }
+  dr::mhp::fence();
+  
+} */
