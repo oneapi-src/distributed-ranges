c1be5eb2abbec76a1ee228f2a7fcdba847d7d890
Robert Cohn <robert.s.cohn@intel.com>
2023-05-23 09:16:04 -0500

    add error checking to stencil benchmarks

--- benchmarks/gbench/mhp/stencil_2d.cpp
+++ benchmarks/gbench/mhp/stencil_2d.cpp
@@ -15,18 +15,103 @@ using T = double;
 
 static const T init_val = 1;
 
+// For debugging, use  col_static = 10 with --vector-size 100
+// const std::size_t cols_static = 10;
 const std::size_t cols_static = 10000;
+
+using Row = std::array<T, cols_static>;
 
 static auto shape() {
   std::size_t rows = default_vector_size / cols_static;
   return std::pair(rows, cols_static);
 }
 
+void print_matrix(rng::forward_range auto &&actual) {
+  auto [rows, cols] = shape();
+  auto m = rng::views::chunk(actual, cols);
+  for (auto row : m) {
+    fmt::print("    ");
+    for (auto e : row) {
+      fmt::print("{:8.3} ", e);
+    }
+    fmt::print("\n");
+  }
+}
+
+struct CommonChecker {
+  void check(rng::forward_range auto &&actual) {
+    if (init) {
+      auto fp_compare = [](auto n1, auto n2) {
+        return fabs(n1 - n2) / n2 < .01;
+      };
+
+      if (rng::equal(actual, expected, fp_compare)) {
+        return;
+      }
+
+      fmt::print("Mismatch\n");
+      if (actual.size() <= 100) {
+        fmt::print("  Expected:\n");
+        print_matrix(expected);
+        fmt::print("  Actual:\n");
+        print_matrix(actual);
+      }
+      exit(1);
+    } else {
+      auto [rows, cols] = shape();
+      expected.resize(rows * cols);
+      rng::copy(actual, expected.begin());
+      init = true;
+    }
+  }
+
+  std::vector<T> expected;
+  bool init = false;
+} common;
+
+struct Checker {
+  void check(rng::forward_range auto &&actual) {
+    if (!check_results || checked) {
+      return;
+    }
+
+    common.check(actual);
+    checked = true;
+  }
+
+  void check_device(sycl::queue q, T *p) {
+    if (!check_results || checked) {
+      return;
+    }
+
+    auto [rows, cols] = shape();
+    auto sz = rows * cols;
+    std::vector<T> local(sz);
+    q.copy(p, local.data(), sz).wait();
+    common.check(local);
+    checked = true;
+  }
+
+  void check_array(rng::forward_range auto &&actual) {
+    if (!check_results || checked) {
+      return;
+    }
+
+    std::vector<Row> local(actual.size());
+    dr::mhp::copy(0, actual, local.begin());
+    auto [rows, cols] = shape();
+    common.check(rng::span(&(local[0][0]), rows * cols));
+    checked = true;
+  }
+
+  bool checked = false;
+};
+
 static void stencil_global_op(auto in, auto out, auto cols, auto i, auto j) {
   out[i * cols + j] =
-      (in[(i - 1) * cols] + in[i * cols + j - 1] + in[i * cols + j] +
+      (in[(i - 1) * cols + j] + in[i * cols + j - 1] + in[i * cols + j] +
        in[i * cols + j + 1] + in[(i + 1) * cols + j]) /
-      5;
+      4;
 }
 
 static void Stencil2D_Loop_Std(benchmark::State &state) {
@@ -40,6 +125,7 @@ static void Stencil2D_Loop_Std(benchmark::State &state) {
   auto in = a.data();
   auto out = b.data();
 
+  Checker checker;
   for (auto _ : state) {
     for (std::size_t i = 0; i < stencil_steps; i++) {
       for (std::size_t i = 1; i < rows - 1; i++) {
@@ -49,6 +135,8 @@ static void Stencil2D_Loop_Std(benchmark::State &state) {
       }
       std::swap(in, out);
     }
+
+    checker.check(rng::span(in, rows * cols));
   }
 }
 
@@ -58,7 +146,7 @@ auto stencil_1darray_op = [](auto &&v) {
   auto &[in_row, out_row] = v;
   auto p = &in_row;
   for (std::size_t i = 1; i < cols_static - 1; i++) {
-    out_row[i] = (p[-1][i] + p[0][i - 1] + p[0][i] + p[0][i + 1] + p[1][i]) / 5;
+    out_row[i] = (p[-1][i] + p[0][i - 1] + p[0][i] + p[0][i + 1] + p[1][i]) / 4;
   }
 };
 
@@ -70,8 +158,6 @@ static void Stencil2D_1DArray_DR(benchmark::State &state) {
   }
   assert(cols == cols_static);
 
-  using Row = std::array<T, cols_static>;
-
   dr::halo_bounds hb(1);
   dr::mhp::distributed_vector<Row> a(rows, hb);
   dr::mhp::distributed_vector<Row> b(rows, hb);
@@ -82,6 +168,7 @@ static void Stencil2D_1DArray_DR(benchmark::State &state) {
   xhp::for_each(a, fill_row);
   xhp::for_each(b, fill_row);
 
+  Checker checker;
   auto in = rng::subrange(a.begin() + 1, a.end() - 1);
   auto out = rng::subrange(b.begin() + 1, b.end() - 1);
   for (auto _ : state) {
@@ -90,6 +177,7 @@ static void Stencil2D_1DArray_DR(benchmark::State &state) {
       dr::mhp::for_each(dr::mhp::views::zip(in, out), stencil_1darray_op);
       std::swap(in, out);
     }
+    checker.check_array(stencil_steps % 2 ? b : a);
   }
 }
 
@@ -113,6 +201,7 @@ static void Stencil2D_Basic_SYCL(benchmark::State &state) {
   q.fill(out, init_val, rows * cols);
   q.wait();
 
+  Checker checker;
   for (auto _ : state) {
     for (std::size_t s = 0; s < stencil_steps; s++) {
       auto op = [=](auto it) {
@@ -121,6 +210,7 @@ static void Stencil2D_Basic_SYCL(benchmark::State &state) {
       q.parallel_for(sycl::range(rows - 2, cols - 2), op).wait();
       std::swap(in, out);
     }
+    checker.check_device(q, in);
   }
 }
 
