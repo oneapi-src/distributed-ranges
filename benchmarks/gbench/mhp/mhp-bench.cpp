127ef501bd251745b3401ba1c2c37804ed03db38
Robert Cohn <robert.s.cohn@intel.com>
2023-05-27 07:39:17 -0400

    Chunk micro-benchmarks, restructure stencil 2d benchmarks (#290)

--- benchmarks/gbench/mhp/mhp-bench.cpp
+++ benchmarks/gbench/mhp/mhp-bench.cpp
@@ -11,6 +11,8 @@ std::size_t comm_size;
 std::size_t default_vector_size;
 std::size_t default_repetitions;
 std::size_t stencil_steps;
+std::size_t num_rows;
+std::size_t num_columns;
 bool check_results;
 
 cxxopts::ParseResult options;
@@ -60,12 +62,14 @@ int main(int argc, char *argv[]) {
   // clang-format off
   options_spec.add_options()
     ("check", "Check results")
+    ("columns", "Number of columns", cxxopts::value<std::size_t>()->default_value("10000"))
     ("drhelp", "Print help")
     ("log", "Enable logging")
 #ifdef SYCL_LANGUAGE_VERSION
     ("sycl", "Execute on SYCL device")
 #endif
     ("reps", "Debug repetitions for short duration vector operations", cxxopts::value<std::size_t>()->default_value("1"))
+    ("rows", "Number of rows", cxxopts::value<std::size_t>()->default_value("10000"))
     ("stencil-steps", "Default steps for stencil", cxxopts::value<std::size_t>()->default_value("100"))
     ("vector-size", "Default vector size", cxxopts::value<std::size_t>()->default_value("100000000"))
     ;
@@ -93,6 +97,8 @@ int main(int argc, char *argv[]) {
   default_vector_size = options["vector-size"].as<std::size_t>();
   default_repetitions = options["reps"].as<std::size_t>();
   stencil_steps = options["stencil-steps"].as<std::size_t>();
+  num_rows = options["rows"].as<std::size_t>();
+  num_columns = options["columns"].as<std::size_t>();
   check_results = options.count("check");
 
   if (comm_rank == 0) {
--- benchmarks/gbench/mhp/stencil_2d.cpp
+++ benchmarks/gbench/mhp/stencil_2d.cpp
@@ -116,7 +116,10 @@ static void stencil_1darray_op(auto in, auto out, auto cols, auto i, auto j) {
       4;
 }
 
-static void Stencil2D_Loop_Std(benchmark::State &state) {
+//
+// Serial baseline
+//
+static void Stencil2D_Loop_Serial(benchmark::State &state) {
   auto [rows, cols] = shape();
   if (rows == 0) {
     return;
@@ -142,9 +145,9 @@ static void Stencil2D_Loop_Std(benchmark::State &state) {
   }
 }
 
-BENCHMARK(Stencil2D_Loop_Std);
+BENCHMARK(Stencil2D_Loop_Serial);
 
-auto stencil_2darray_op = [](auto &&v) {
+auto stencil_foreach_stdArray_op = [](auto &&v) {
   auto &[in_row, out_row] = v;
   auto p = &in_row;
   for (std::size_t i = 1; i < cols_static - 1; i++) {
@@ -152,7 +155,10 @@ auto stencil_2darray_op = [](auto &&v) {
   }
 };
 
-static void Stencil2D_StdArray_DR(benchmark::State &state) {
+//
+// Distributed vector of std::array
+//
+static void Stencil2D_ForeachStdArray_DR(benchmark::State &state) {
   auto [rows, cols] = shape();
 
   if (rows == 0) {
@@ -178,16 +184,21 @@ static void Stencil2D_StdArray_DR(benchmark::State &state) {
   for (auto _ : state) {
     for (std::size_t s = 0; s < stencil_steps; s++) {
       dr::mhp::halo(in).exchange();
-      dr::mhp::for_each(dr::mhp::views::zip(in, out), stencil_2darray_op);
+      dr::mhp::for_each(dr::mhp::views::zip(in, out),
+                        stencil_foreach_stdArray_op);
       std::swap(in, out);
     }
     checker.check_array(stencil_steps % 2 ? b : a);
   }
 }
 
-BENCHMARK(Stencil2D_StdArray_DR);
+BENCHMARK(Stencil2D_ForeachStdArray_DR);
 
-static void Stencil2D_1DArray_DR(benchmark::State &state) {
+//
+// Distributed vector of floats. Granularity ensures segments contain
+// whole rows. Explicitly process segments SPMD-style.
+//
+static void Stencil2D_Nocollective_DR(benchmark::State &state) {
   auto [rows, cols] = shape();
   if (rows == 0) {
     return;
@@ -198,22 +209,19 @@ static void Stencil2D_1DArray_DR(benchmark::State &state) {
   dr::mhp::distributed_vector<T> b(rows * cols, init_val, dist);
 
   Checker checker;
-  auto in = rng::subrange(a.begin() + cols, a.end() - cols);
-  auto out = rng::subrange(b.begin() + cols, b.end() - cols);
+  auto in =
+      dr::mhp::local_segment(rng::subrange(a.begin() + cols, a.end() - cols));
+  auto out =
+      dr::mhp::local_segment(rng::subrange(b.begin() + cols, b.end() - cols));
+  auto size = rng::size(in);
+  assert(size % cols == 0);
+  auto row_slice = size / cols;
   for (auto _ : state) {
     for (std::size_t s = 0; s < stencil_steps; s++) {
-      auto in_segment = dr::mhp::local_segment(in);
-      auto out_segment = dr::mhp::local_segment(out);
-      auto size = rng::size(in_segment);
-      assert(size % cols == 0);
-      auto row_slice = size / cols;
-
-      dr::mhp::halo(in).exchange();
-
+      dr::mhp::halo(stencil_steps % 2 ? b : a).exchange();
       for (std::size_t i = 0; i < row_slice; i++) {
         for (std::size_t j = 1; j < cols - 1; j++) {
-          stencil_1darray_op(in_segment.begin(), out_segment.begin(), cols, i,
-                             j);
+          stencil_1darray_op(in.begin(), out.begin(), cols, i, j);
         }
       }
       std::swap(in, out);
@@ -222,8 +230,62 @@ static void Stencil2D_1DArray_DR(benchmark::State &state) {
   }
 }
 
-BENCHMARK(Stencil2D_1DArray_DR);
+BENCHMARK(Stencil2D_Nocollective_DR);
 
+// Under construction
+#if 0
+auto nslice(auto &&r, cols) {
+  auto slice = [](auto &&chunk) {
+    return rng::subrange(chunk.begin() + 1, chunk.end() - 1);
+  };
+  return r | rng::views::chunk(cols)
+    | rng::views::transform(slice)
+    | rng::views::join;
+}
+
+//
+// Slice implemented by views. Use for_each on flat representation
+//
+static void Stencil2D_1DArrayTransform_DR(benchmark::State &state) {
+  auto v = shape();
+  auto rows = std::get<0>(v);
+  auto cols = std::get<0>(v);
+
+  if (rows == 0) {
+    return;
+  }
+
+  auto op = [cols](auto &center) {
+    auto p = &center;
+    return (p[-cols] + p[-1] + p[0] + p[+1] + p[cols]) / 4;
+  }
+
+  auto dist = dr::mhp::distribution().halo(cols).granularity(cols);
+  dr::mhp::distributed_vector<T> a(rows * cols, init_val, dist);
+  dr::mhp::distributed_vector<T> b(rows * cols, init_val, dist);
+
+  Checker checker;
+  auto in_global = rng::subrange(a.begin() + cols, a.end() - cols);
+  auto out_global = rng::subrange(b.begin() + cols, b.end() - cols);
+  for (auto _ : state) {
+    for (std::size_t s = 0; s < stencil_steps; s++) {
+      auto in_local = dr::mhp::local_segment(in);
+      auto out_local = dr::mhp::local_segment(out);
+
+      dr::mhp::halo(in_local).exchange();
+      dr::mhp::transform(nslice(in_local, cols), nslice(out_local, cols), op);
+      std::swap(in, out);
+    }
+    checker.check(stencil_steps % 2 ? b : a);
+  }
+}
+
+BENCHMARK(Stencil2D_1DArrayTransform_DR);
+#endif
+
+//
+// Single process SYCL baseline
+//
 #ifdef SYCL_LANGUAGE_VERSION
 static void Stencil2D_Basic_SYCL(benchmark::State &state) {
   auto s = shape();
