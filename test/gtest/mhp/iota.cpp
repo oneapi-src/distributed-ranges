54a17eb775a2e0fba44dbb9591bbc4f8df4fcd7e
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-06-01 15:39:32 +0200

    tests for dr::views::iota added

---
+++ test/gtest/mhp/iota.cpp
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "xhp-tests.hpp"
+
+TEST(IotaView, ZipWithDR) {
+  dr::mhp::distributed_vector<int> dv(20);
+  auto v = dr::views::iota(1, 20);
+  int ref = 1;
+
+  auto z = dr::mhp::views::zip(dv, v);
+  for (auto itr = z.begin(); itr != z.end(); itr++) {
+    auto [dve, ve] = *itr;
+    dve = ve;
+    EXPECT_EQ(dv[ref - 1], ref);
+    ref++;
+  }
+}
+
+TEST(IotaView, Copy) {
+  dr::mhp::distributed_vector<int> dv(10);
+  auto v = dr::views::iota(1, 11);
+
+  rng::copy(v, dv.begin());
+
+  EXPECT_TRUE(equal(dv, std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+}
+
+TEST(IotaView, Transform) {
+  dr::mhp::distributed_vector<int> dv(10);
+  auto v = dr::views::iota(1, 11);
+  auto negate = [](auto v) { return -v; };
+
+  rng::transform(v, dv.begin(), negate);
+
+  EXPECT_TRUE(
+      equal(dv, std::vector<int>{-1, -2, -3, -4, -5, -6, -7, -8, -9, -10}));
+}
+
+TEST(IotaView, ForEach) {
+  dr::mhp::distributed_vector<int> dv(10);
+  auto v = dr::views::iota(1, 11);
+
+  auto negate = [](auto v) {
+    auto &[in, out] = v;
+    out = -in;
+  };
+
+  auto z = dr::mhp::views::zip(v, dv);
+
+  rng::for_each(z, negate);
+
+  EXPECT_TRUE(
+      equal(dv, std::vector<int>{-1, -2, -3, -4, -5, -6, -7, -8, -9, -10}));
+}
+
+TYPED_TEST(IotaView, ReduceRootRange) {
+  auto v = dr::views::iota(1, 11);
+  auto root = 0;
+
+  auto result = dr::mhp::reduce(root, v, 0, std::plus{});
+
+  if (comm_rank == root) {
+    EXPECT_EQ(55, result);
+  }
+}
