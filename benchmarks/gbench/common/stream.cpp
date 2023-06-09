89fcbe119c2a4a127572dff74ff2f1e9201432a1
Suyash Bakshi <suyash.bakshi@intel.com>
2023-06-09 11:16:10 -0500

    Add black scholes to gtest in stream.cpp

--- benchmarks/gbench/common/stream.cpp
+++ benchmarks/gbench/common/stream.cpp
@@ -80,3 +80,44 @@ static void Stream_Triad(benchmark::State &state) {
 }
 
 DR_BENCHMARK(Stream_Triad);
+
+T normalCDF(T value)
+{
+   return 0.5 * std::erfc(-value * M_SQRT1_2);
+}
+
+static void Black_Scholes(benchmark::State &state) {
+  T scalar = val;
+  T r = 0;
+  T sig = 0;
+
+  xhp::distributed_vector<T> s0(default_vector_size, scalar);
+  xhp::distributed_vector<T> x(default_vector_size, scalar);
+  xhp::distributed_vector<T> t(default_vector_size, scalar);
+  xhp::distributed_vector<T> vcall(default_vector_size, scalar);
+  xhp::distributed_vector<T> vput(default_vector_size, scalar);
+
+  Stats stats(state, sizeof(T) * (s0.size() + x.size()), sizeof(T) * (vcall.size() + vput.size()));
+
+  auto black_scholes =
+    [=](auto&& e) {
+      auto&& [s0, x, t, vcall, vput] = e;
+      T d1 = ( std::log(s0/x) + (r + T(0.5)*sig*sig)*t ) /
+                ( sig*std::sqrt(t) );
+      T d2 = ( std::log(s0/x) + (r - T(0.5)*sig*sig)*t ) /
+                ( sig*std::sqrt(t) );
+      vcall = s0*normalCDF(d1) - std::exp(-r*t)*x*normalCDF(d2);
+      vput = std::exp(-r*t)*x*normalCDF(-d2) - s0*normalCDF(-d1);
+  };
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      
+      xhp::for_each(xhp::views::zip(s0, x, t, vcall, vput), black_scholes);
+
+    }
+  }
+}
+
+DR_BENCHMARK(Black_Scholes);
