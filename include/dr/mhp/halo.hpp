88671a4c5c796864eb9e432d6154eb5c3acaa3f7
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-06-12 14:49:37 +0200

    mhp::halo exchange with prev!=next bug fixed

--- include/dr/mhp/halo.hpp
+++ include/dr/mhp/halo.hpp
@@ -363,11 +363,11 @@ private:
     drlog.debug(nostd::source_location::current(),
                 "owned groups {}/{} first/last\n", comm.first(), comm.last());
     if (hb.prev > 0 && (hb.periodic || !comm.first())) {
-      owned.emplace_back(span.subspan(hb.prev, hb.prev), comm.prev(),
+      owned.emplace_back(span.subspan(hb.next, hb.next), comm.prev(),
                          communicator::tag::halo_reverse);
     }
     if (hb.next > 0 && (hb.periodic || !comm.last())) {
-      owned.emplace_back(span.subspan(rng::size(span) - 2 * hb.next, hb.next),
+      owned.emplace_back(span.subspan(rng::size(span) - 2 * hb.prev, hb.prev),
                          comm.next(), communicator::tag::halo_forward);
     }
     return owned;
