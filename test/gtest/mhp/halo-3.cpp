70eb6dc5a5d37532c0262cf0534d84934008516c
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-06-29 21:38:42 +0200

    license information added to new files

--- test/gtest/mhp/halo-3.cpp
+++ test/gtest/mhp/halo-3.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include "xhp-tests.hpp"
 
 template <typename T> class Halo3 : public testing::Test {};
