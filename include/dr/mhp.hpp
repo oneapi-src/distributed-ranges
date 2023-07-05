a66fae746f9fd069248623a2eb1124667709c006
Łukasz Ślusarczyk <112692748+lslusarczyk@users.noreply.github.com>
2023-07-05 15:26:52 +0200

    Inc scan (#349)
    * inclusive scan for mhp
    
    ---------
    
    Co-authored-by: Kacper Stefanski <kacper.stefanski@intel.com>

--- include/dr/mhp.hpp
+++ include/dr/mhp.hpp
@@ -67,6 +67,7 @@ namespace fmt {}
 #include <dr/mhp/algorithms/copy.hpp>
 #include <dr/mhp/algorithms/fill.hpp>
 #include <dr/mhp/algorithms/for_each.hpp>
+#include <dr/mhp/algorithms/inclusive_scan.hpp>
 #include <dr/mhp/algorithms/iota.hpp>
 #include <dr/mhp/algorithms/reduce.hpp>
 #include <dr/mhp/algorithms/stencil_for_each.hpp>
