53bdd883ad966464032569ef3684015211e5296c
Robert Cohn <robert.s.cohn@intel.com>
2023-07-09 10:08:19 -0500

    Turn on ctest failure output on devcloud

--- scripts/devcloud-run.sh
+++ scripts/devcloud-run.sh
@@ -7,5 +7,8 @@
 set -e
 hostname
 source /opt/intel/oneapi/setvars.sh
+# default sycl device will be GPU, if available
+export CTEST_OUTPUT_ON_FAILURE=1
 make -C build test
+# another run, forcing CPU
 ONEAPI_DEVICE_SELECTOR=opencl:cpu make -C build test
