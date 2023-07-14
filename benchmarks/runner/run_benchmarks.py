6035f426affe214bb141df809295c23e35386cd6
Kacper Stefanski <kacper.stefanski@intel.com>
2023-07-14 15:59:34 +0200

    reduced the number of parameters for run_mhp and run_shp
    added click options to insert common parameters via cli

--- benchmarks/runner/run_benchmarks.py
+++ benchmarks/runner/run_benchmarks.py
@@ -11,19 +11,27 @@ import os
 import datetime
 import click
 
+def bench_common_params(func):
+    func = click.option('--vec_size', default=1000000, type=int, help='Size of a vector')(func)
+    func = click.option('--reps', default=100, type=int, help='Number of reps')(func)
+    func = click.option('--bench_filter', default='Stream_', type=str, help='A filter used for a benchmark')(func)
+    return func
+
 @click.command()
 @click.option('--dry_run', default=False, help='Emits commands but does not execute')
 def print_run_command(command:str, dry_run:bool):
-    print(f"Current benchmark command used: \n{command}")
+    click.echo(f"Current benchmark command used: \n{command}")
     if not dry_run:
         output = subprocess.run(
             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
     )
 
+@click.command()
+@bench_common_params
 def run_mhp(
     vec_size: int,
     reps: int,
-    filter: str,
+    bench_filter: str,
     sycl_used: bool,
     mpirun: bool = True,
     n: int = None,
@@ -31,9 +39,7 @@ def run_mhp(
     pin_order: str = '',
     pin_cell: str = '',
 ):
-
     bench_path = "./benchmarks/gbench/mhp/mhp-bench"
-
     time_now_string = datetime.datetime.now().isoformat(timespec='minutes')
     directory_path = f"benchmarks/json_output/mhp"
     if not os.path.exists(directory_path):
@@ -49,18 +55,19 @@ def run_mhp(
         mpirun = ""
         n = ""
     if sycl_used:
-        command = f"BENCHMARK_FILTER={filter} I_MPI_PIN_DOMAIN={pin_domain} I_MPI_PIN_ORDER={pin_order} I_MPI_PIN_CELL={pin_cell} {mpirun} {n} {bench_path} --sycl --vector-size {str(vec_size)} --reps {str(reps)} {bench_out}"
+        command = f"BENCHMARK_FILTER={bench_filter} I_MPI_PIN_DOMAIN={pin_domain} I_MPI_PIN_ORDER={pin_order} I_MPI_PIN_CELL={pin_cell} {mpirun} {n} {bench_path} --sycl --vector-size {str(vec_size)} --reps {str(reps)} {bench_out}"
     else:
-        command = f"BENCHMARK_FILTER={filter} I_MPI_PIN_DOMAIN={pin_domain} I_MPI_PIN_ORDER={pin_order} I_MPI_PIN_CELL={pin_cell} {mpirun} {n} {bench_path} --vector-size {str(vec_size)} --reps {str(reps)} {bench_out}"
+        command = f"BENCHMARK_FILTER={bench_filter} I_MPI_PIN_DOMAIN={pin_domain} I_MPI_PIN_ORDER={pin_order} I_MPI_PIN_CELL={pin_cell} {mpirun} {n} {bench_path} --vector-size {str(vec_size)} --reps {str(reps)} {bench_out}"
 
     print_run_command(command)
 
-
+@click.command()
+@bench_common_params
 def run_shp(
     vec_size: int,
     reps: int,
     d: int,
-    filter: str = '',
+    bench_filter: str = '',
     kmp_aff: str = '',
 ):
     bench_path = "./benchmarks/gbench/shp/shp-bench"
@@ -69,29 +76,25 @@ def run_shp(
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
     bench_out = f"--benchmark_out={directory_path}/shp_benchmark_{time_now_string}.json --benchmark_out_format=json"
-    command = f"BENCHMARK_FILTER={filter} KMP_AFFINITY={kmp_aff} {bench_path} -d {str(d)} --vector-size {str(vec_size)} --reps {str(reps)} {bench_out}"
+    command = f"BENCHMARK_FILTER={bench_filter} KMP_AFFINITY={kmp_aff} {bench_path} -d {str(d)} --vector-size {str(vec_size)} --reps {str(reps)} {bench_out}"
     print_run_command(command)
 
-
 def run_all_benchmarks_fsycl_O3():
-    vec_size = 1000000000
-    reps = 100
-    bench_filter = "Stream_"
     kmp_affinity = "compact"
 
     # shp
     d = 1
-    run_shp(vec_size, reps, d)
-    run_shp(vec_size, reps, d, bench_filter, kmp_affinity)
+    run_shp(d=d)
+    run_shp(d=d, kmp_aff=kmp_affinity)
 
     d = 2
-    run_shp(vec_size, reps, d)
-    run_shp(vec_size, reps, d, bench_filter, kmp_affinity)
+    run_shp(d=d)
+    run_shp(d=d, kmp_aff=kmp_affinity)
 
     # mhp/cpu
     sycl_used = False
     mpirun = False
-    run_mhp(vec_size, reps, bench_filter, sycl_used, mpirun=False)
+    run_mhp(sycl_used=sycl_used, mpirun=False)
     n = 24
     mpi_pin_domain = "core"
     mpi_pin_order = "compact"
@@ -99,27 +102,21 @@ def run_all_benchmarks_fsycl_O3():
 
     mpirun = True
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        mpirun,
-        n,
-        mpi_pin_domain,
-        mpi_pin_order,
-        mpi_pin_cell,
+        sycl_used=sycl_used,
+        mpirun=mpirun,
+        n=n,
+        pin_domain=mpi_pin_domain,
+        pin_order=mpi_pin_order,
+        pin_cell=mpi_pin_cell,
     )
     n = 48
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        mpirun,
-        n,
-        mpi_pin_domain,
-        mpi_pin_order,
-        mpi_pin_cell,
+        sycl_used=sycl_used,
+        mpirun=mpirun,
+        n=n,
+        pin_domain=mpi_pin_domain,
+        pin_order=mpi_pin_order,
+        pin_cell=mpi_pin_cell,
     )
 
     # mhp/sycl
@@ -127,40 +124,30 @@ def run_all_benchmarks_fsycl_O3():
     sycl_used = True
     n = 1
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        n,
-        mpi_pin_domain,
+        sycl_used=sycl_used,
+        n=n,
+        pin_domain=mpi_pin_domain,
         mpirun=mpirun,
     )
-    run_mhp(vec_size, reps, bench_filter, sycl_used, n, mpirun=mpirun)
+    run_mhp(sycl_used=sycl_used, n=n, mpirun=mpirun)
     n = 2
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        n,
-        mpi_pin_domain,
-        mpi_pin_order,
-        mpi_pin_cell,
+        sycl_used=sycl_used,
+        n=n,
+        pin_domain=mpi_pin_domain,
+        pin_order=mpi_pin_order,
+        pin_cell=mpi_pin_cell,
         mpirun=mpirun,
     )
 
-
 def run_all_benchmarks_other_options():
-    vec_size = 1000000000
-    reps = 100
-    bench_filter = "Stream_"
     kmp_affinity = "compact"
     mpirun = True
     # shp
     d = 1
-    run_shp(vec_size, reps, d, bench_filter, kmp_affinity)
+    run_shp(d=d, kmp_aff=kmp_affinity)
     d = 2
-    run_shp(vec_size, reps, d, bench_filter, kmp_affinity)
+    run_shp(d=d, kmp_aff=kmp_affinity)
     # mhp/cpu
     sycl_used = False
     mpi_pin_domain = "core"
@@ -168,67 +155,37 @@ def run_all_benchmarks_other_options():
     mpi_pin_cell = "unit"
     n = 24
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        mpirun,
-        n,
-        mpi_pin_domain,
-        mpi_pin_order,
-        mpi_pin_cell,
+        sycl_used=sycl_used,
+        mpirun=mpirun,
+        n=n,
+        pin_domain=mpi_pin_domain,
+        pin_order=mpi_pin_order,
+        pin_cell=mpi_pin_cell,
     )
     n = 48
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        mpirun,
-        n,
-        mpi_pin_domain,
-        mpi_pin_order,
-        mpi_pin_cell,
+        sycl_used=sycl_used,
+        mpirun=mpirun,
+        n=n,
+        pin_domain=mpi_pin_domain,
+        pin_order=mpi_pin_order,
+        pin_cell=mpi_pin_cell,
     )
 
-
-# def save_to_csv(self, output):
-#     rows = output.splitlines()
-#     rows = [row.split(',') for row in rows]
-
-#     with open(self.csv_path, 'a', newline='') as f:
-#         writer = csv.writer(f)
-#         for row in rows:
-#             writer.writerow(row)
-
-# def extract_json_save_csv(filepath:str):
-#     file = json.load(filepath)
-
-
 def run_mhp_test():
-    vec_size = 1000000000
-    reps = 100
-    bench_filter = "Stream_"
-    kmp_affinity = "compact"
     sycl_used = False
     mpirun = False
     n = 1
     run_mhp(
-        vec_size,
-        reps,
-        bench_filter,
-        sycl_used,
-        mpirun,
-        n,
+        sycl_used=sycl_used,
+        mpirun=mpirun,
+        n=n,
     )
 
 
 def run_shp_test():
-    vec_size = 1000000000
-    reps = 100
     d = 1
-
-    run_shp(vec_size, reps, d)
+    run_shp(d=d)
 
 
 run_mhp_test()
--- scripts/devcloud-daily.sh
+++ scripts/devcloud-daily.sh
@@ -6,11 +6,10 @@
 source /opt/intel/oneapi/setvars.sh
 set -e
 hostname
-export CXX=icpx 
-cmake -DENABLE_SYCL:BOOL=ON -DENABLE_SYCL_MPI:BOOL=ON -B build -S .
+CXX=icpx cmake -DENABLE_SYCL:BOOL=ON -DENABLE_SYCL_MPI:BOOL=ON -B build -S .
 make -C build -j
 
 # python -m pip install --upgrade pip
 cd build
 # pip install -r requirements.txt
-python3 ../benchmarks/runner/run_benchmarks.py
+python3 ../benchmarks/runner/run_benchmarks.py --vec_size=1000000 --reps=100 --bench_filter='Stream_'
