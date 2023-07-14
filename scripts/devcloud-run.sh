4c017a5e4c733c43acb00abaf15b31160e9c52a9
Robert Cohn <robert.s.cohn@intel.com>
2023-07-14 13:49:40 -0400

    use 1 GPU on devcloud (#381)

--- scripts/devcloud-run.sh
+++ scripts/devcloud-run.sh
@@ -11,7 +11,8 @@ source /opt/intel/oneapi/setvars.sh
 # we clear the environment to make mpi work, add back error output
 export CTEST_OUTPUT_ON_FAILURE=1
 
-# default sycl device will be GPU, if available
-make -C build test
+# default sycl device will be GPU, if available. Only use 1 GPU:
+# workaround for devcloud multi-card not working
+ONEAPI_DEVICE_SELECTOR=level_zero:0 make -C build test
 # another run, forcing CPU
 ONEAPI_DEVICE_SELECTOR=opencl:cpu make -C build test
