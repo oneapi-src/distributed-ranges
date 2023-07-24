5635d86a819ff7a022b696b95af3e30c9eeb64b5
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-07-24 11:52:41 +0000

    Code unification on communicator class

--- test/gtest/mhp/communicator.cpp
+++ test/gtest/mhp/communicator.cpp
@@ -26,8 +26,27 @@ TEST(Communicator, Alltoallv) {
     senddispl[i] = recvdispl[i] = i * SIZE;
   }
 
-  dr::mhp::default_comm().alltoallv(vec_src, sendsizes, senddispl, vec_dst,
-                                    recvsizes, recvdispl);
+  dr::mhp::default_comm().alltoallv(vec_src.data(), sendsizes, senddispl,
+                                    vec_dst.data(), recvsizes, recvdispl);
+
+  std::vector<T> vec_ref(comm_size * SIZE);
+
+  for (std::size_t i = 0; i < comm_size; i++)
+    for (std::size_t j = 0; j < SIZE; j++) {
+      vec_ref[i * SIZE + j] = 10 * i + 1;
+    }
+
+  EXPECT_TRUE(equal(vec_ref, vec_dst));
+}
+
+TEST(Communicator, Allgather) {
+  const std::size_t SIZE = 2;
+  std::vector<T> vec_src(SIZE);
+  std::vector<T> vec_dst(comm_size * SIZE);
+
+  rng::fill(vec_src, comm_rank * 10 + 1);
+
+  dr::mhp::default_comm().all_gather(vec_src, vec_dst);
 
   std::vector<T> vec_ref(comm_size * SIZE);
 
