c87cccfd419d994756b76736b8fca87e3bc12114
Tuomas Karna <tuomas.karna@intel.com>
2023-07-26 20:28:28 +0300

    wave kernel tests: skip parallel tests + nicer formatting

--- test/gtest/mhp/wave_kernel.cpp
+++ test/gtest/mhp/wave_kernel.cpp
@@ -11,13 +11,12 @@ const int nx = 3, ny = 3;
 const std::size_t halo_radius = 1;
 auto dist = dr::mhp::distribution().halo(halo_radius);
 
-bool equal(MDA &a, std::initializer_list<T> expected) {
+bool equal(MDA &a, std::vector<T> expected) {
   bool result = true;
-  std::vector<T> v(expected);
   std::size_t k = 0;
   for (std::size_t i = 0; i < a.mdspan().extent(0); i++) {
     for (std::size_t j = 0; j < a.mdspan().extent(1); j++) {
-      result *= a.mdspan()(i, j) == v[k];
+      result *= a.mdspan()(i, j) == expected[k];
       k++;
     }
   }
@@ -61,25 +60,50 @@ MDA generate_v() {
 }
 
 TEST(MhpTests, WaveKernelCreateE) {
+  if (comm_size > 1)
+    return;
   MDA a = generate_e();
-  EXPECT_TRUE(equal(a, {0, 0, 0, 11, 12, 13, 12, 13, 14, 13, 14, 15}))
+  // first row is unused (all arrays must have the same number of rows)
+  // clang-format off
+  EXPECT_TRUE(equal(a, { 0,  0,  0,
+                        11, 12, 13,
+                        12, 13, 14,
+                        13, 14, 15}))
       << fmt::format("E:\n{}\n", a);
+  // clang-format on
 }
 
 TEST(MhpTests, WaveKernelCreateU) {
+  if (comm_size > 1)
+    return;
   MDA a = generate_u();
-  EXPECT_TRUE(equal(a, {10, 11, 12, 11, 12, 13, 12, 13, 14, 13, 14, 15}))
+  // clang-format off
+  EXPECT_TRUE(equal(a, {10, 11, 12,
+                        11, 12, 13,
+                        12, 13, 14,
+                        13, 14, 15}))
       << fmt::format("U:\n{}\n", a);
+  // clang-format on
 }
 
 TEST(MhpTests, WaveKernelCreateV) {
+  if (comm_size > 1)
+    return;
   MDA a = generate_v();
+  // first row is unused (all arrays must have the same number of rows)
+  // clang-format off
   EXPECT_TRUE(
-      equal(a, {0, 0, 0, 0, 11, 12, 13, 14, 12, 13, 14, 15, 13, 14, 15, 16}))
+      equal(a, { 0,  0,  0,  0,
+                11, 12, 13, 14,
+                12, 13, 14, 15,
+                13, 14, 15, 16}))
       << fmt::format("V:\n{}\n", a);
+  // clang-format on
 }
 
 TEST(MhpTests, WaveKernelDeDx) {
+  if (comm_size > 1)
+    return;
   MDA e = generate_e();
   MDA dedx({nx + 1, ny}, dist);
   dr::mhp::fill(dedx, 0.0);
@@ -101,11 +125,18 @@ TEST(MhpTests, WaveKernelDeDx) {
   dr::mhp::stencil_for_each(rhs_dedx, e_view, dedx_view);
 
   // first/last rows are boundary data, only interior values are computed
-  EXPECT_TRUE(equal(dedx, {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0}))
+  // clang-format off
+  EXPECT_TRUE(equal(dedx, {0, 0, 0,
+                           1, 1, 1,
+                           1, 1, 1,
+                           0, 0, 0}))
       << fmt::format("dedx:\n{}\n", dedx);
+  // clang-format on
 }
 
 TEST(MhpTests, WaveKernelDeDy) {
+  if (comm_size > 1)
+    return;
   MDA e = generate_e();
   MDA dedy({nx + 1, ny + 1}, dist);
   dr::mhp::fill(dedy, 0.0);
@@ -123,13 +154,20 @@ TEST(MhpTests, WaveKernelDeDy) {
   auto dedy_view = dr::mhp::views::submdspan(dedy.view(), start, end);
   dr::mhp::stencil_for_each(rhs_dedy, e_view, dedy_view);
 
-  // first row is unused (number of rows must be the same for all arrays)
+  // first row is unused (all arrays must have the same number of rows)
   // first/last cols are boundary data, only interior values are computed
-  EXPECT_TRUE(equal(dedy, {0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0}))
+  // clang-format off
+  EXPECT_TRUE(equal(dedy, {0, 0, 0, 0,
+                           0, 1, 1, 0,
+                           0, 1, 1, 0,
+                           0, 1, 1, 0}))
       << fmt::format("dedy:\n{}\n", dedy);
+  // clang-format on
 }
 
 TEST(MhpTests, WaveKernelDuDx) {
+  if (comm_size > 1)
+    return;
   MDA u = generate_u();
   MDA dudx({nx + 1, ny}, dist);
   dr::mhp::fill(dudx, 0.0);
@@ -146,12 +184,19 @@ TEST(MhpTests, WaveKernelDuDx) {
   auto dudx_view = dr::mhp::views::submdspan(dudx.view(), start, end);
   dr::mhp::stencil_for_each(rhs_dudx, u_view, dudx_view);
 
-  // first row is unused (number of rows must be the same for all arrays)
-  EXPECT_TRUE(equal(dudx, {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1}))
+  // first row is unused (all arrays must have the same number of rows)
+  // clang-format off
+  EXPECT_TRUE(equal(dudx, {0, 0, 0,
+                           1, 1, 1,
+                           1, 1, 1,
+                           1, 1, 1}))
       << fmt::format("dudx:\n{}\n", dudx);
+  // clang-format on
 }
 
 TEST(MhpTests, WaveKernelDvDy) {
+  if (comm_size > 1)
+    return;
   MDA v = generate_v();
   MDA dvdy({nx + 1, ny}, dist);
   dr::mhp::fill(dvdy, 0.0);
@@ -168,7 +213,12 @@ TEST(MhpTests, WaveKernelDvDy) {
   auto dvdy_view = dr::mhp::views::submdspan(dvdy.view(), start, end);
   dr::mhp::stencil_for_each(rhs_dvdy, v_view, dvdy_view);
 
-  // first row is unused (number of rows must be the same for all arrays)
-  EXPECT_TRUE(equal(dvdy, {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1}))
+  // first row is unused (all arrays must have the same number of rows)
+  // clang-format off
+  EXPECT_TRUE(equal(dvdy, {0, 0, 0,
+                           1, 1, 1,
+                           1, 1, 1,
+                           1, 1, 1}))
       << fmt::format("dvdy:\n{}\n", dvdy);
+  // clang-format on
 }
