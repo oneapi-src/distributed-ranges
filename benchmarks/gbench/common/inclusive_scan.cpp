6e9ae59cc16322d7557cedc2b8053825859e8f7d
Robert Cohn <robert.s.cohn@intel.com>
2023-08-01 15:16:51 -0500

    udpate

---
+++ benchmarks/gbench/common/inclusive_scan.cpp
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "../common/dr_bench.hpp"
+
+using T = double;
+
+static void Inclusive_Scan_DR(benchmark::State &state) {
+  xhp::distributed_vector<T> a(default_vector_size, 3);
+  xhp::distributed_vector<T> b(default_vector_size, 0);
+  Stats stats(state, sizeof(T) * a.size(), sizeof(T) * b.size());
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      xhp::inclusive_scan(a, b);
+    }
+  }
+}
+
+DR_BENCHMARK(Inclusive_Scan_DR);
+
+static void Inclusive_Scan_DPL(benchmark::State &state) {
+  sycl::queue q;
+  auto policy = oneapi::dpl::execution::make_device_policy(q);
+  auto a = sycl::malloc_device<T>(default_vector_size, q);
+  auto b = sycl::malloc_device<T>(default_vector_size, q);
+  Stats stats(state, sizeof(T) * default_vector_size, sizeof(T) * default_vector_size);
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      std::inclusive_scan(policy, a, a + default_vector_size, b);
+    }
+  }
+}
+
+DR_BENCHMARK(Inclusive_Scan_DPL);
+
+static void Inclusive_Scan_Serial(benchmark::State &state) {
+  std::vector<T> a(default_vector_size, 3);
+  std::vector<T> b(default_vector_size, 0);
+  Stats stats(state, sizeof(T) * a.size(), sizeof(T) * b.size());
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      std::inclusive_scan(std::execution::par_unseq, rng::begin(a), rng::end(a),
+                          rng::begin(b));
+    }
+  }
+}
+
+DR_BENCHMARK(Inclusive_Scan_Serial);
