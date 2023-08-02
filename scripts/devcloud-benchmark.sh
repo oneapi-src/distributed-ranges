baa22debbcad88b0584aedc284c8dcd0c6a579f1
Robert Cohn <robert.s.cohn@intel.com>
2023-08-02 09:19:44 -0400

    build run on same devcloud node because the same oneapi install may not be available (#442)

--- scripts/devcloud-benchmark.sh
+++ scripts/devcloud-benchmark.sh
@@ -6,4 +6,9 @@
 source /opt/intel/oneapi/setvars.sh
 set -e
 hostname
+# devcloud requires --launcher=fork for mpi
+cmake -B build -DENABLE_SYCL=on -DENABLE_MPIFORK=on
+# parallel build
+cmake --build build -j --target mhp-bench --target shp-bench
+# serial build
 cmake --build build --target devcloud-bench
