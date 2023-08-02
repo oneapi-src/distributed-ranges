dc18e711690e9eec98b298b2ca59dc36fcd27cc4
Robert Cohn <robert.s.cohn@intel.com>
2023-08-02 07:46:59 -0500

    update

--- scripts/devcloud-test.sh
+++ scripts/devcloud-test.sh
@@ -12,6 +12,6 @@ cmake -B build -DENABLE_SYCL=on -DENABLE_MPIFORK=on
 
 # default sycl device will be GPU, if available. Only use 1 GPU:
 # workaround for devcloud multi-card not working
-ONEAPI_DEVICE_SELECTOR=level_zero:0 make -C build all test
+ONEAPI_DEVICE_SELECTOR=level_zero:0 make -j -C build all test
 # another run, forcing CPU
-ONEAPI_DEVICE_SELECTOR=opencl:cpu make -C build all test
+ONEAPI_DEVICE_SELECTOR=opencl:cpu make -j -C build all test
