c36b5aa329f8558c4f5ad59ac6e2bd02f7b35741
Robert Cohn <robert.s.cohn@intel.com>
2023-08-09 16:56:27 -0500

    cpu working

--- include/dr/mhp/algorithms/inclusive_scan.hpp
+++ include/dr/mhp/algorithms/inclusive_scan.hpp
@@ -15,44 +15,49 @@ template <dr::distributed_contiguous_range R, dr::distributed_iterator O,
           typename BinaryOp, typename U = rng::range_value_t<R>>
 auto inclusive_scan_impl_(R &&r, O &&d_first, BinaryOp &&binary_op,
                           std::optional<U> init = {}) {
-  using value_type = U;
   assert(aligned(r, d_first));
 
-  auto input_segments = local_segments(r);
-  auto output_segments = local_segments(d_first);
-
+  using value_type = U;
+  auto comm = default_comm();
+  bool first_rank = comm.rank() == 0;
+  auto io_segments =
+      rng::views::zip(local_segments(r), local_segments(d_first));
   std::vector<value_type> segment_bases;
-  for (auto io_segment : rng::views::zip(input_segments, output_segments)) {
-    auto [in_segment, out_segment] = io_segment;
-    // fmt::print("in segment: {}\n", in_segment);
-    std::inclusive_scan(std::execution::par_unseq, in_segment.begin(),
-                        in_segment.end(), out_segment.begin(), binary_op);
-    segment_bases.push_back(out_segment.back());
-    // fmt::print("out segment: {}\n", out_segment);
-  }
 
-  // fmt::print("segment bases in: {}\n", segment_bases);
+  bool first_segment = true;
+  for (auto io_segment : io_segments) {
+    auto [in_segment, out_segment] = io_segment;
+    dr::drlog.debug("in segment: {}\n", in_segment);
+    if (init && first_rank && first_segment) {
+      std::inclusive_scan(std::execution::par_unseq, in_segment.begin(),
+                          in_segment.end(), out_segment.begin(), binary_op,
+                          init.value());
+    } else {
+      std::inclusive_scan(std::execution::par_unseq, in_segment.begin(),
+                          in_segment.end(), out_segment.begin(), binary_op);
+    }
+    segment_bases.push_back(out_segment.back());
+    first_segment = false;
+    dr::drlog.debug("out segment: {}\n", out_segment);
+  }
+  segment_bases.push_back(value_type{});
+
+  dr::drlog.debug("segment bases in: {}\n", segment_bases);
   //  scan segment bases
-  std::inclusive_scan(segment_bases.begin(), segment_bases.end(),
-                      segment_bases.begin(), binary_op);
-  // fmt::print("segment bases out: {}\n", segment_bases);
+  std::exclusive_scan(segment_bases.begin() + 1, segment_bases.end(),
+                      segment_bases.begin() + 1, *segment_bases.begin(),
+                      binary_op);
+  dr::drlog.debug("segment bases out: {}\n", segment_bases);
 
   // gather ranks to root
-  auto comm = default_comm();
-  std::vector<value_type> rank_bases(comm.size());
+  std::vector<value_type> rank_bases(comm.size() + 1);
   comm.gather(segment_bases.back(), std::span{rank_bases}, 0);
 
-  // fmt::print("rank bases in: {}\n", rank_bases);
+  dr::drlog.debug("rank bases in: {}\n", rank_bases);
   //  scan rank bases
-  if (init) {
-    std::inclusive_scan(std::execution::par_unseq, rank_bases.begin(),
-                        rank_bases.end(), rank_bases.begin(), binary_op,
-                        init.value());
-  } else {
-    std::inclusive_scan(std::execution::par_unseq, rank_bases.begin(),
-                        rank_bases.end(), rank_bases.begin(), binary_op);
-  }
-  // fmt::print("rank bases out: {}\n", rank_bases);
+  std::exclusive_scan(rank_bases.begin() + 1, rank_bases.end(),
+                      rank_bases.begin() + 1, *rank_bases.begin(), binary_op);
+  dr::drlog.debug("rank bases out: {}\n", rank_bases);
 
   // scatter rank bases
   value_type rank_base;
@@ -60,18 +65,27 @@ auto inclusive_scan_impl_(R &&r, O &&d_first, BinaryOp &&binary_op,
 
   // rebase local segments
   std::size_t i = 0;
-  for (auto segment : output_segments) {
-    // fmt::print("segment before rebase: {}\n", segment);
-    std::for_each(std::execution::par_unseq, rng::begin(segment),
-                  rng::end(segment),
-                  [binary_op, base = rank_base + segment_bases[i]](auto &v) {
-                    v = binary_op(v, base);
-                  });
-    // fmt::print("segment after rebase: {}\n", segment);
+  first_segment = true;
+  for (auto io_segment : io_segments) {
+    auto [in_segment, out_segment] = io_segment;
+    auto segment = out_segment | rng::views::take(rng::size(in_segment));
+
+    dr::drlog.debug("segment before rebase: {}\n", segment);
+    auto base = rank_base;
+    if (!first_rank || !first_segment) {
+      if (!first_segment) {
+        base = binary_op(base, segment_bases[i++]);
+      }
+
+      std::for_each(std::execution::par_unseq, rng::begin(segment),
+                    rng::end(segment),
+                    [binary_op, base](auto &v) { v = binary_op(v, base); });
+    }
+    first_segment = false;
+    dr::drlog.debug("segment after rebase: {}\n", segment);
   }
 
   barrier();
-
   return d_first + rng::size(r);
 }
 
