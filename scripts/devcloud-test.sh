a1ff700fa6fa15258a7540522dce37cb1a9c57b8
Robert Cohn <robert.s.cohn@intel.com>
2023-08-13 11:05:18 -0400

    update devcloud (#474)

--- scripts/devcloud-test.sh
+++ scripts/devcloud-test.sh
@@ -4,17 +4,14 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# system bashrc does this
-# source /opt/intel/oneapi/setvars.sh
-
+source /opt/intel/oneapi/setvars.sh
 set -e
 hostname
-
-# devcloud requires --launcher=fork for mpi
-cmake -B build -DENABLE_SYCL=on -DENABLE_MPIFORK=on
+cmake -B build -DENABLE_SYCL=on
 
 # default sycl device will be GPU, if available. Only use 1 GPU:
 # workaround for devcloud multi-card not working
 ONEAPI_DEVICE_SELECTOR=level_zero:0 make -j -C build all test
+
 # another run, forcing CPU
 ONEAPI_DEVICE_SELECTOR=opencl:cpu make -j -C build all test
