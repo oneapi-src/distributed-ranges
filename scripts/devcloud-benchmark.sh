17dc692990513df755fb7f31daa92f9521109f40
Robert Cohn <robert.s.cohn@intel.com>
2023-08-13 11:00:53 -0500

    update

--- scripts/devcloud-benchmark.sh
+++ scripts/devcloud-benchmark.sh
@@ -7,5 +7,10 @@
 source /opt/intel/oneapi/setvars.sh
 set -e
 hostname
+
+# SLURM/MPI integration is broken
+unset SLURM_TASKS_PER_NODE
+unset SLURM_JOBID
+
 cmake -B build -DENABLE_SYCL=on
 cmake --build build -j --target devcloud-bench
