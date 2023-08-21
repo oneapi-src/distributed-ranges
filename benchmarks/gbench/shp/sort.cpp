0d27cde2d8f1c2974c4af86daa05743662792a76
Kacper Stefanski <kacper.stefanski@intel.com>
2023-08-21 18:24:13 +0200

    added filling with random values

--- benchmarks/gbench/shp/sort.cpp
+++ benchmarks/gbench/shp/sort.cpp
@@ -6,8 +6,15 @@
 
 using T = float;
 
+template <rng::forward_range X> void fill_random(X &&x) {
+  for (auto &&value : x) {
+    value = drand48() * 100;
+  }
+}
+
 static void Sort_DR(benchmark::State &state) {
-  dr::shp::distributed_vector<T> a(default_vector_size, 3);
+  dr::shp::distributed_vector<T> a(default_vector_size);
+  fill_random(a);
   Stats stats(state, sizeof(T) * a.size());
   for (auto _ : state) {
     for (std::size_t i = 0; i < default_repetitions; i++) {
@@ -23,7 +30,10 @@ DR_BENCHMARK(Sort_DR);
 static void Sort_DPL(benchmark::State &state) {
   auto q = get_queue();
   auto policy = oneapi::dpl::execution::make_device_policy(q);
+  std::vector<T> a_local(default_vector_size);
+  fill_random(a_local);
   auto a = sycl::malloc_device<T>(default_vector_size, q);
+  q.memcpy(a, a_local.data(), default_vector_size * sizeof(T)).wait();
   Stats stats(state, sizeof(T) * default_vector_size);
 
   for (auto _ : state) {
