7be4cc32762c5fde50de00f780abe0d6adb3157d
Kacper Stefanski <56044375+haichangsi@users.noreply.github.com>
2023-08-25 14:09:12 +0200

    added the sort DPL version using sort_async (#513)

--- benchmarks/gbench/common/sort.cpp
+++ benchmarks/gbench/common/sort.cpp
@@ -25,7 +25,7 @@ static void Sort_DR(benchmark::State &state) {
 DR_BENCHMARK(Sort_DR);
 
 #ifdef SYCL_LANGUAGE_VERSION
-static void Sort_DPL(benchmark::State &state) {
+static void Sort_EXP(benchmark::State &state) {
   auto q = get_queue();
   auto policy = oneapi::dpl::execution::make_device_policy(q);
   std::vector<T> a_local(default_vector_size);
@@ -37,6 +37,29 @@ static void Sort_DPL(benchmark::State &state) {
   for (auto _ : state) {
     stats.rep();
     std::sort(policy, a, a + default_vector_size);
+  }
+  sycl::free(a, q);
+}
+
+DR_BENCHMARK(Sort_EXP);
+
+static void Sort_DPL(benchmark::State &state) {
+  auto q = get_queue();
+  auto policy = oneapi::dpl::execution::make_device_policy(q);
+  std::vector<T> a_local(default_vector_size);
+  fill_random(a_local);
+  auto a = sycl::malloc_device<T>(default_vector_size, q);
+  q.memcpy(a, a_local.data(), default_vector_size * sizeof(T)).wait();
+  std::span<T> d_a(a, default_vector_size);
+  dr::__detail::direct_iterator d_first(d_a.begin());
+  dr::__detail::direct_iterator d_last(d_a.end());
+  Stats stats(state, sizeof(T) * default_vector_size);
+
+  for (auto _ : state) {
+    stats.rep();
+    oneapi::dpl::experimental::sort_async(policy, d_first, d_last,
+                                          std::less<>{})
+        .wait();
   }
   sycl::free(a, q);
 }
