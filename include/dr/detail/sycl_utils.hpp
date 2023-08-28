99683933eb669543fe195dd9465932f908c156ad
Robert Cohn <robert.s.cohn@intel.com>
2023-08-28 15:32:14 -0500

    extend parallel_for workarounds to 2d

--- include/dr/detail/sycl_utils.hpp
+++ include/dr/detail/sycl_utils.hpp
@@ -6,6 +6,8 @@
 
 #include <limits>
 
+#include <dr/detail/mdspan_utils.hpp>
+
 #ifdef SYCL_LANGUAGE_VERSION
 
 #include <sycl/sycl.hpp>
@@ -50,34 +52,60 @@ inline std::size_t max_kernel_size_(std::size_t block_size) {
 // This is a workaround to avoid performance degradation
 // in DPC++ for odd range sizes.
 template <typename Fn>
-sycl::event parallel_for_workaround(sycl::queue &q, sycl::range<1> numWorkItems,
+sycl::event parallel_for_workaround(sycl::queue &q, sycl::range<1> range,
                                     Fn &&fn) {
   auto block_size = workgroup_shapes[1][0];
-  std::size_t num_blocks = (numWorkItems.size() + block_size - 1) / block_size;
+  std::size_t num_blocks = (range.size() + block_size - 1) / block_size;
 
-  int32_t range_size = numWorkItems.size();
+  int32_t range_size = range.size();
 
-  auto event = q.parallel_for(
-      sycl::nd_range<>(num_blocks * block_size, block_size), [=](auto nd_idx) {
-        auto idx = nd_idx.get_global_id(0);
-        if (idx < range_size) {
-          fn(idx);
-        }
-      });
-  return event;
+  return q.parallel_for(sycl::nd_range<>(num_blocks * block_size, block_size),
+                        [=](auto nd_idx) {
+                          auto idx = nd_idx.get_global_id(0);
+                          if (idx < range_size) {
+                            fn(idx);
+                          }
+                        });
+}
+
+// This is a workaround to avoid performance degradation
+// in DPC++ for odd range sizes.
+template <typename Fn>
+sycl::event parallel_for_workaround(sycl::queue &q, sycl::range<2> flat_range,
+                                    Fn &&fn) {
+  auto workgroup_shape = workgroup_shapes[2];
+  sycl::range<2> rounded_range(round_up(flat_range[0], workgroup_shape[0]),
+                               round_up(flat_range[1], workgroup_shape[1]));
+  sycl::range<2> block_size(workgroup_shape[0], workgroup_shape[1]);
+
+  sycl::nd_range<2> nd_range(rounded_range, block_size);
+
+  return q.parallel_for(nd_range, [=](auto nd_idx) {
+    std::array idx = {nd_idx.get_global_id(0), nd_idx.get_global_id(1)};
+    if (idx[0] < flat_range[0] && idx[1] < flat_range[1]) {
+      fn(idx);
+    }
+  });
+}
+
+auto combine_events(sycl::queue &q, const auto &events) {
+  return q.submit([&](auto &&h) {
+    h.depends_on(events);
+    // Empty host task necessary due to [CMPLRLLVM-46542]
+    h.host_task([] {});
+  });
 }
 
 template <typename Fn>
-sycl::event parallel_for_64bit(sycl::queue &q, sycl::range<1> numWorkItems,
-                               Fn &&fn) {
+sycl::event parallel_for_64bit(sycl::queue &q, sycl::range<1> range, Fn &&fn) {
   auto block_size = workgroup_shapes[1][0];
   std::size_t max_kernel_size = max_kernel_size_(block_size);
 
   std::vector<sycl::event> events;
-  for (std::size_t base_idx = 0; base_idx < numWorkItems.size();
+  for (std::size_t base_idx = 0; base_idx < range.size();
        base_idx += max_kernel_size) {
     std::size_t launch_size =
-        std::min(numWorkItems.size() - base_idx, max_kernel_size);
+        std::min(range.size() - base_idx, max_kernel_size);
 
     auto e = parallel_for_workaround(q, sycl::range<1>(launch_size),
                                      [=](sycl::id<1> idx_) {
@@ -88,13 +116,16 @@ sycl::event parallel_for_64bit(sycl::queue &q, sycl::range<1> numWorkItems,
     events.push_back(e);
   }
 
-  auto e = q.submit([&](auto &&h) {
-    h.depends_on(events);
-    // Empty host task necessary due to [CMPLRLLVM-46542]
-    h.host_task([] {});
-  });
+  return combine_events(q, events);
+}
 
-  return e;
+template <typename Fn>
+sycl::event parallel_for_64bit(sycl::queue &q, sycl::range<2> range, Fn &&fn) {
+  std::vector<sycl::event> events;
+  assert(false);
+
+  return combine_events(q, events);
+  ;
 }
 
 template <int Dim, typename Fn>
--- test/gtest/common/sycl_utils.cpp
+++ test/gtest/common/sycl_utils.cpp
@@ -22,7 +22,32 @@ TEST(SYCLUtils, ParalelFor1D) {
   q.parallel_for(range, seta).wait();
   dr::__detail::parallel_for(q, range, setb).wait();
 
-  EXPECT_NE(rng::span(a, size), rng::span(a, size));
+  EXPECT_EQ(rng::span(a, size), rng::span(b, size));
+}
+
+void set(auto col_size, auto base, auto index) {
+  base[(index[0] + 1) * col_size + index[1] + 1] = 22;
+}
+
+TEST(SYCLUtils, ParalelFor2D) {
+  const std::size_t row_size = 5, col_size = row_size,
+                    size = row_size * col_size;
+  sycl::queue q;
+  sycl::range<2> range(row_size - 2, col_size - 2);
+
+  auto a = sycl::malloc_shared<T>(size, q);
+  auto b = sycl::malloc_shared<T>(size, q);
+  std::fill(a, a + size, 99);
+  std::fill(b, b + size, 99);
+  auto seta = [col_size, base = a](auto index) { set(col_size, base, index); };
+  auto setb = [col_size, base = b](auto index) { set(col_size, base, index); };
+
+  q.parallel_for(range, seta).wait();
+  dr::__detail::parallel_for(q, range, setb).wait();
+
+  EXPECT_EQ(rng::span(a, size), rng::span(b, size))
+      << fmt::format("a:\n{}b:\n{}", md::mdspan(a, row_size, col_size),
+                     md::mdspan(b, row_size, col_size));
 }
 
 #endif // SYCL_LANGUAGE_VERSION
