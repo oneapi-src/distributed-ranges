27ecac20bfdd68b8d9793fb30c08297742ee675c
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-09-12 16:29:44 +0200

    tests update

---
+++ test/gtest/mhp/mhpsort.cpp
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "xhp-tests.hpp"
+
+using T = int;
+using DV = xhp::distributed_vector<T>;
+using LV = std::vector<T>;
+
+TEST(MhpSort, BigRandom) {
+  LV v = generate_random<T>(1234567, 2048);
+  auto size = v.size();
+  DV d_v(size);
+
+  dr::mhp::copy(0, v, d_v.begin());
+  barrier();
+
+  std::sort(v.begin(), v.end());
+  xhp::sort(d_v);
+
+  barrier();
+  EXPECT_TRUE(equal(v, d_v));
+}
