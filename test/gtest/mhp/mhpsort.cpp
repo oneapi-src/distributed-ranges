44f3354f415d52d73da0e3d03225ff6deae9c0f2
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-09-13 16:59:38 +0200

    test size change

--- test/gtest/mhp/mhpsort.cpp
+++ test/gtest/mhp/mhpsort.cpp
@@ -9,7 +9,7 @@ using DV = xhp::distributed_vector<T>;
 using LV = std::vector<T>;
 
 TEST(MhpSort, BigRandom) {
-  LV v = generate_random<T>(1000000, 2048);
+  LV v = generate_random<T>(1200000, 2048);
   auto size = v.size();
   DV d_v(size);
 
@@ -17,7 +17,7 @@ TEST(MhpSort, BigRandom) {
   barrier();
 
   std::sort(v.begin(), v.end());
-  xhp::sort(d_v);
+  dr::mhp::sort(d_v);
 
   barrier();
   EXPECT_TRUE(equal(v, d_v));
