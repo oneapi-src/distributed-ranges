451ab17d6b279f79d7ccfb71e1117f9b2fe85138
Łukasz Ślusarczyk <lukasz.slusarczyk@intel.com>
2023-11-07 13:16:52 +0100

    reverted accidental change

--- test/gtest/mhp/mhp-tests.cpp
+++ test/gtest/mhp/mhp-tests.cpp
@@ -31,15 +31,13 @@ void dr_init() {
 }
 
 int main(int argc, char *argv[]) {
-  comm = MPI_COMM_WORLD;
-
   MPI_Init(&argc, &argv);
+  comm = MPI_COMM_WORLD;
   int rank, size;
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &size);
   comm_rank = rank;
   comm_size = size;
-
   ::testing::InitGoogleTest(&argc, argv);
 
   cxxopts::Options options_spec(argv[0], "DR MHP tests");
