a00b6ab5013a5fd1b6ee44452905c1ab103671cd
Łukasz Ślusarczyk <112692748+lslusarczyk@users.noreply.github.com>
2023-12-30 21:22:36 +0100

    added GPU tests (#228)

---
+++ scripts/run_command_on_compute_node.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+set -e
+
+# needed for tasks running under PBS
+cd ${PBS_O_WORKDIR:-.}
+
+# needed for tasks running under SLURM, as SLURM/MPI integration is broken
+unset SLURM_TASKS_PER_NODE
+unset SLURM_JOBID
+
+echo "Host: " $(hostname)
+echo "CWD: " $(pwd)
+module list
+
+if [ "$#" -le 1 ]; then
+	echo "provide as first argument a log file"
+	exit 1
+fi
+
+LOGFILE=$1
+mkdir -p $(dirname $LOGFILE)
+
+shift 1
+
+"$@" 2>&1 > $LOGFILE
