44b4c51c75378ec9f0a4df843d7a30222fcba577
Łukasz Ślusarczyk <112692748+lslusarczyk@users.noreply.github.com>
2024-01-31 12:36:44 +0100

    stream moved to separate file (#696)

---
+++ benchmarks/gbench/common/stream.hpp
@@ -0,0 +1,78 @@
+// SPDX-FileCopyrightText: Intel Corporation
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include "dr_bench.hpp"
+
+template <class ContainerT> void Stream_Copy(benchmark::State &state) {
+  using T = rng::value_type_t<ContainerT>;
+  T init = 0;
+  ContainerT a(default_vector_size, init);
+  ContainerT b(default_vector_size, init);
+  Stats stats(state, sizeof(T) * a.size(), sizeof(T) * b.size());
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      xhp::for_each(xhp::views::zip(a, b),
+                    [](auto &&v) { std::get<1>(v) = std::get<0>(v); });
+    }
+  }
+}
+
+inline int val = 0;
+
+template <class ContainerT> void Stream_Scale(benchmark::State &state) {
+  using T = rng::value_type_t<ContainerT>;
+  T scalar = val;
+  ContainerT a(default_vector_size, scalar);
+  ContainerT b(default_vector_size, scalar);
+  Stats stats(state, sizeof(T) * a.size(), sizeof(T) * b.size());
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      xhp::for_each(xhp::views::zip(a, b), [scalar](auto &&v) {
+        std::get<1>(v) = scalar * std::get<0>(v);
+      });
+    }
+  }
+}
+
+template <class ContainerT> void Stream_Add(benchmark::State &state) {
+  using T = rng::value_type_t<ContainerT>;
+  T scalar = val;
+  xhp::distributed_vector<T> a(default_vector_size, scalar);
+  xhp::distributed_vector<T> b(default_vector_size, scalar);
+  xhp::distributed_vector<T> c(default_vector_size, scalar);
+  Stats stats(state, sizeof(T) * (a.size() + b.size()), sizeof(T) * c.size());
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      xhp::for_each(xhp::views::zip(a, b, c), [](auto &&v) {
+        std::get<2>(v) = std::get<0>(v) + std::get<1>(v);
+      });
+    }
+  }
+}
+
+template <class ContainerT> void Stream_Triad(benchmark::State &state) {
+  using T = rng::value_type_t<ContainerT>;
+  T scalar = val;
+  ContainerT a(default_vector_size, scalar);
+  ContainerT b(default_vector_size, scalar);
+  ContainerT c(default_vector_size, scalar);
+  Stats stats(state, sizeof(T) * (a.size() + b.size()), sizeof(T) * c.size());
+
+  for (auto _ : state) {
+    for (std::size_t i = 0; i < default_repetitions; i++) {
+      stats.rep();
+      xhp::for_each(xhp::views::zip(a, b, c), [scalar](auto &&v) {
+        std::get<2>(v) = std::get<0>(v) + scalar * std::get<1>(v);
+      });
+    }
+  }
+}
