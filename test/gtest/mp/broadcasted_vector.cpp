f17243bc1d873c248f89503f0f8d88daa2f49be9
Mikolaj Komar <mikolaj.komar@intel.com>
2024-12-09 11:57:09 +0100

    Fix formatting

--- test/gtest/mp/broadcasted_vector.cpp
+++ test/gtest/mp/broadcasted_vector.cpp
@@ -7,28 +7,28 @@
 TEST(BroadcastedVector, BroadcastData) {
   std::size_t n = 100;
   auto rank = dr::mp::default_comm().rank();
-  std::vector<int>  data(n);
+  std::vector<int> data(n);
   if (rank == 0) {
     for (int i = 0; i < n; i++) {
-        data[i] = i;
+      data[i] = i;
     }
   }
   dr::mp::broadcasted_vector<int> broadcasted;
   if (rank == 0) {
-    broadcasted.broadcast_data(n, 0, data,
-                             dr::mp::default_comm());
-  }
-  else {
+    broadcasted.broadcast_data(n, 0, data, dr::mp::default_comm());
+  } else {
     broadcasted.broadcast_data(n, 0, rng::empty_view<int>(),
-                             dr::mp::default_comm());
+                               dr::mp::default_comm());
   }
-  
+
   std::vector<int> ref(n);
   for (int i = 0; i < n; i++) {
     ref[i] = i;
   }
 
-  EXPECT_EQ(rng::subrange(broadcasted.broadcasted_data(), broadcasted.broadcasted_data() + n), ref);
+  EXPECT_EQ(rng::subrange(broadcasted.broadcasted_data(),
+                          broadcasted.broadcasted_data() + n),
+            ref);
   broadcasted.destroy_data();
 }
 
@@ -38,35 +38,35 @@ TEST(BroadcastedVector, BroadcastDataReuse) {
   std::vector<int> data(n);
   if (rank == 0) {
     for (int i = 0; i < n; i++) {
-        data[i] = i;
+      data[i] = i;
     }
   }
   dr::mp::broadcasted_vector<int> broadcasted;
   if (rank == 0) {
-    broadcasted.broadcast_data(n, 0, data,
-                             dr::mp::default_comm());
-  }
-  else {
+    broadcasted.broadcast_data(n, 0, data, dr::mp::default_comm());
+  } else {
     broadcasted.broadcast_data(n, 0, rng::empty_view<int>(),
-                             dr::mp::default_comm());
+                               dr::mp::default_comm());
   }
-  
+
   std::vector<int> ref(n);
   for (int i = 0; i < n; i++) {
     ref[i] = i;
   }
 
-  EXPECT_EQ(rng::subrange(broadcasted.broadcasted_data(), broadcasted.broadcasted_data() + n), ref);
+  EXPECT_EQ(rng::subrange(broadcasted.broadcasted_data(),
+                          broadcasted.broadcasted_data() + n),
+            ref);
   broadcasted.destroy_data();
   EXPECT_EQ(broadcasted.broadcasted_data(), nullptr);
-    if (rank == 0) {
-    broadcasted.broadcast_data(n, 0, data,
-                             dr::mp::default_comm());
-  }
-  else {
+  if (rank == 0) {
+    broadcasted.broadcast_data(n, 0, data, dr::mp::default_comm());
+  } else {
     broadcasted.broadcast_data(n, 0, rng::empty_view<int>(),
-                             dr::mp::default_comm());
+                               dr::mp::default_comm());
   }
-  EXPECT_EQ(rng::subrange(broadcasted.broadcasted_data(), broadcasted.broadcasted_data() + n), ref);
+  EXPECT_EQ(rng::subrange(broadcasted.broadcasted_data(),
+                          broadcasted.broadcasted_data() + n),
+            ref);
   broadcasted.destroy_data();
 }
