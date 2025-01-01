46f6adea7a810cc065b308369126aea911d1ac99
Filip Głębocki <fg429202@students.mimuw.edu.pl>
2025-01-01 23:53:55 +0100

    prog

--- include/dr/mp/containers/dual_distributed_vector.hpp
+++ include/dr/mp/containers/dual_distributed_vector.hpp
@@ -290,6 +290,9 @@ private:
     std::size_t segment_index = 0;
     // bool first_half = true;
     for (std::size_t i = 0; i < size; i += segment_size_) {
+      std::cout << i << ": segments_.emplace_back(" << segment_index 
+        << ", " << std::min(segment_size_, size - i) 
+        << ", " << data_size_ << ")\n";
       segments_.emplace_back(this, segment_index++,
                              std::min(segment_size_, size - i), data_size_);
 
