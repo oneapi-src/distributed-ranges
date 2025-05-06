b517be9da167b7b436c9419a92cc285e3a51a4f8
Filip Głębocki <fg429202@students.mimuw.edu.pl>
2025-05-06 22:56:44 +0200

    prog

--- test/gtest/mp/halo-dual.cpp
+++ test/gtest/mp/halo-dual.cpp
@@ -213,7 +213,7 @@ TYPED_TEST(HaloDual, local_is_accessible_in_halo_region_halo_11__partial) {
 // perf test!
 
 static constexpr size_t DISTRIBUTED_VECTOR_SIZE = 100000;
-// static constexpr size_t N_STEPS = 100000;
+static constexpr size_t N_STEPS = 100000;
 // // auto stencil1d_subrange_op = [](auto &center) {
 // //   auto win = &center;
 // //   return win[-1] + win[0] + win[1];
@@ -314,15 +314,17 @@ auto is_local = [](const auto &segment) {
 };
 
 void perf_test_dual_segment() {
-  dr::mp::dual_distributed_vector<int> dv(1000 * DISTRIBUTED_VECTOR_SIZE, dr::mp::distribution().halo(1, 1));
+  dr::mp::dual_distributed_vector<int> dv(100 * DISTRIBUTED_VECTOR_SIZE, dr::mp::distribution().halo(1, 1));
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  for (auto &seg : dr::ranges::segments(dv) | rng::views::filter(is_local)) {
-    auto b = dr::ranges::local(rng::begin(seg));
-    auto s = rng::subrange(b, b + rng::distance(seg));
-
-    rng::for_each(s, [](auto &center) { return center + 1; });
+  for (size_t i = 0; i < N_STEPS; i++) {
+    for (auto &seg : dr::ranges::segments(dv) | rng::views::filter(is_local)) {
+      auto b = dr::ranges::local(rng::begin(seg));
+      auto s = rng::subrange(b, b + rng::distance(seg));
+  
+      rng::for_each(s, [](auto &center) { return center + 1; });
+    }
   }
 
   auto end = std::chrono::high_resolution_clock::now();
@@ -331,15 +333,17 @@ void perf_test_dual_segment() {
 }
 
 void perf_test_classic_segment() {
-  dr::mp::distributed_vector<int> dv(1000 * DISTRIBUTED_VECTOR_SIZE, dr::mp::distribution().halo(1, 1));
+  dr::mp::distributed_vector<int> dv(100 * DISTRIBUTED_VECTOR_SIZE, dr::mp::distribution().halo(1, 1));
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  for (auto &seg : dr::ranges::segments(dv) | rng::views::filter(is_local)) {
-    auto b = dr::ranges::local(rng::begin(seg));
-    auto s = rng::subrange(b, b + rng::distance(seg));
-
-    rng::for_each(s, [](auto &center) { return center + 1; });
+  for (size_t i = 0; i < N_STEPS; i++) {
+    for (auto &seg : dr::ranges::segments(dv) | rng::views::filter(is_local)) {
+      auto b = dr::ranges::local(rng::begin(seg));
+      auto s = rng::subrange(b, b + rng::distance(seg));
+  
+      rng::for_each(s, [](auto &center) { return center + 1; });
+    }
   }
 
   auto end = std::chrono::high_resolution_clock::now();
