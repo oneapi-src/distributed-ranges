c261441df817027200df4d669c7a37498441974a
Filip Głębocki <fg429202@students.mimuw.edu.pl>
2025-08-19 01:49:41 +0200

    prog

--- test/gtest/mp/halo-dual.cpp
+++ test/gtest/mp/halo-dual.cpp
@@ -237,7 +237,7 @@ static constexpr size_t N_STEPS = 100;
 static constexpr size_t N_KERNEL_STEPS = 100;
 
 [[maybe_unused]]
-static constexpr bool DO_RAMPING_TESTS = false;
+static constexpr bool DO_RAMPING_TESTS = true;
 
 [[maybe_unused]]
 static constexpr size_t NON_RAMPING_RETRIES = 1;
