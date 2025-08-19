166971cb18008ca2c34ca8b2c41914ea86caeb04
Filip Głębocki <fg429202@students.mimuw.edu.pl>
2025-08-19 02:27:04 +0200

    prog

--- test/gtest/mp/halo-dual.cpp
+++ test/gtest/mp/halo-dual.cpp
@@ -222,7 +222,7 @@ TYPED_TEST(HaloDual, local_is_accessible_in_halo_region_halo_11__partial) {
 [[maybe_unused]]
 static constexpr size_t DISTRIBUTED_VECTOR_SIZE = 1000000;
 [[maybe_unused]]
-static constexpr size_t HALO_SIZE = 524288;
+static constexpr size_t HALO_SIZE = 2048;
 
 // [[maybe_unused]]
 // static constexpr size_t DISTRIBUTED_VECTOR_SIZE = 100000000;
@@ -234,7 +234,7 @@ static constexpr size_t HALO_SIZE = 524288;
 static constexpr size_t N_STEPS = 100;
 
 [[maybe_unused]]
-static constexpr size_t N_KERNEL_STEPS = 100;
+static constexpr size_t N_KERNEL_STEPS = 2048;
 
 [[maybe_unused]]
 static constexpr bool DO_RAMPING_TESTS = true;
