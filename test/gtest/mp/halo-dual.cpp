74a5cec7c3175ed2c77095ef97a1e3c153c67d56
Filip Głębocki <fg429202@students.mimuw.edu.pl>
2025-09-04 00:22:04 +0200

    prog

--- test/gtest/mp/halo-dual.cpp
+++ test/gtest/mp/halo-dual.cpp
@@ -222,7 +222,7 @@ static constexpr size_t DISTRIBUTED_VECTOR_SIZE = 1000000;
 static constexpr size_t HALO_SIZE = 32;
 
 [[maybe_unused]]
-static constexpr size_t N_STEPS = 100;
+static constexpr size_t N_STEPS = 1000;
 
 [[maybe_unused]]
 static constexpr size_t N_KERNEL_STEPS = 512;
